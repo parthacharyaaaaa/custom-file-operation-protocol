e2fe8b96ac22062f8043156774e624dfa47addbc
Parth Acharya <1008parth@gmail.com>
2025-06-24 12:49:46 +0530

    Updates response model to include class factory methods to create ResponseHeader instances from ProtocolException

--- server/models/response_models.py
+++ server/models/response_models.py
@@ -1,14 +1,17 @@
 '''Module for defining schema of outgoing responses'''
 from pydantic import BaseModel, IPvAnyAddress, model_validator, Field
-from typing import Annotated, Optional, Union
+from typing import Annotated, Optional, Union, Any
 from server import response_codes
+from server.config import ServerConfig
+from server.models.request_model import BaseHeaderComponent
 from datetime import datetime
+from server.errors import ProtocolException
 
 class ResponseHeader(BaseModel):
     version: Annotated[int, Field(ge=0)]
     code: str
     sender_address: Annotated[IPvAnyAddress, Field(frozen=True)]
-    sequence_number: Annotated[int, Field(frozen=True, ge=1)]
+    sequence_number: Annotated[int, Field(frozen=True, ge=0)]
     sender_timestamp: datetime
     body_size: Annotated[int, Field(frozen=True, default=0)]
     description: Annotated[str, Field(max_length=64)]
@@ -20,6 +23,30 @@ class ResponseHeader(BaseModel):
             if self.code in response_category:
                 return True
         return False
+    
+    @classmethod
+    def from_protocol_exception(cls, exc: type[ProtocolException], context_request: BaseHeaderComponent, end_conn: bool = False, body_size: Optional[int] = None, sequence_number: Optional[int] = None, **kwargs) -> 'ResponseHeader':
+        return cls(version=context_request.version,
+                   code=exc.code,
+                   sender_address=f'{ServerConfig.HOST}:{ServerConfig.PORT}',
+                   sequence_number=sequence_number or context_request.sequence_number+1,
+                   sender_timestamp=datetime.now(),
+                   body_size=body_size or 0,
+                   description=exc.description,
+                   end_connection=end_conn,
+                   kwargs=kwargs)
+    
+    @classmethod
+    def from_unverifiable_header(cls, exc: type[ProtocolException], version: Optional[int] = None, seq_num: Optional[int] = None, end_conn: Optional[bool] = False, **kwargs) -> 'ResponseHeader':
+        return cls(version=version or ServerConfig.VERSION,
+                   code=exc.code,
+                   description=exc.description,
+                   sender_address=f'{ServerConfig.HOST}:{ServerConfig.PORT}',
+                   sequence_number=seq_num or 0,
+                   sender_timestamp=datetime.now(),
+                   body_size=0,
+                   end_connection=end_conn,
+                   kwargs=kwargs)
 
 class ResponseBody(BaseModel):
     data: Union[bytes, str]
