7aea573f3c1f936573907b7af33afd0fe237b1a1
Parth Acharya <1008parth@gmail.com>
2025-06-24 15:13:13 +0530

    Adds missing type hints

--- server/dispatch.py
+++ server/dispatch.py
@@ -1,9 +1,9 @@
 from server.config import CategoryFlag
-from types import MappingProxyType
+from types import MappingProxyType, FunctionType
 from server.authz.auth_handler import auth_handler
 from server.file_ops.permission_handler import permission_handler
 from server.file_ops.file_handler import file_handler
 
-TOP_LEVEL_REQUEST_MAPPING: MappingProxyType = MappingProxyType({CategoryFlag.AUTH : auth_handler,
-                                                                CategoryFlag.FILE_OP : file_handler,
-                                                                CategoryFlag.PERMISSION : permission_handler})
+TOP_LEVEL_REQUEST_MAPPING: MappingProxyType[int, FunctionType] = MappingProxyType({CategoryFlag.AUTH : auth_handler,
+                                                                                   CategoryFlag.FILE_OP : file_handler,
+                                                                                   CategoryFlag.PERMISSION : permission_handler})
