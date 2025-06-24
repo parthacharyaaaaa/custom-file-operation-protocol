79bc24ce11961822d8795b21188ecdaf5356c709
Parth Acharya <1008parth@gmail.com>
2025-06-24 23:59:39 +0530

    Reworks ALL models

--- server/models/request_model.py
+++ server/models/request_model.py
@@ -1,12 +1,12 @@
 '''Module for defining schema of incoming requests'''
 from pydantic import BaseModel, Field, model_validator, IPvAnyAddress
-from typing import Annotated, Optional, Literal
+from typing import Annotated, Optional, Literal, Union
 from datetime import datetime
-from server.config import CategoryFlag, ServerConfig
+from server.config import CategoryFlag, PermissionFlag, ServerConfig
 
 class BaseAuthComponent(BaseModel):
-    identity: Annotated[str, Field(min_length=8, max_length=64)]
-    password: Optional[Annotated[str, Field(min_length=16, max_length=1024, default=None)]]
+    identity: Annotated[str, Field(min_length=ServerConfig.USERNAME_RANGE.value[0], max_length=ServerConfig.USERNAME_RANGE.value[1], pattern=ServerConfig.USERNAME_REGEX.value)]
+    password: Optional[Annotated[str, Field(min_length=ServerConfig.PASSWORD_RANGE.value[0], max_length=ServerConfig.PASSWORD_RANGE.value[1], default=None)]]
     token: Optional[Annotated[str, Field(min_length=16, max_length=1024, default=None)]]
     refresh_digest: Optional[Annotated[str, Field(min_length=ServerConfig.REFRESH_DIGEST_LENGTH.value, max_length=ServerConfig.REFRESH_DIGEST_LENGTH.value, frozen=True, default=None)]]
 
@@ -14,17 +14,24 @@ class BaseAuthComponent(BaseModel):
     def auth_semantic_check(self) -> 'BaseAuthComponent':
         if not (self.password or self.token):
             raise ValueError('Passkey or token required for authentication')
+        if self.password and self.token:
+            raise ValueError('Both password and token provided — only one allowed')
         if self.refresh_digest and not self.token:
             raise ValueError('Refresh digest provided but no active token given')
         return self
     
 class BaseFileComponent(BaseModel):
-    subject_file: Annotated[str, Field(max_length=1024, pattern=r'[\w\-\,\@\#\$\%\&\*\(\)\[\]\<\>]{1,64}\.+[a-zA-z]')]
+    # Target file
+    subject_file: Annotated[str, Field(max_length=1024, pattern=ServerConfig.FILENAME_REGEX.value)]
+
+    # Sequencing logic
     chunk_number: Optional[Annotated[int, Field(ge=0, frozen=True, default=None)]]
-    chunk_size: Optional[Annotated[int, Field(ge=1, le=ServerConfig.CHUNK_MAX_SIZE.value, default=ServerConfig.CHUNK_MAX_SIZE.value)]]
-    write_data: Optional[Annotated[str, Field(min_length=1, max_length=ServerConfig.CHUNK_MAX_SIZE.value, frozen=True)]]
-    return_partial: Annotated[bool, Field(default=True)]
-    cursor_keepalive: Annotated[bool, Field(default=False)]
+    chunk_size: Optional[Annotated[int, Field(ge=1, le=ServerConfig.CHUNK_MAX_SIZE.value, default=ServerConfig.CHUNK_MAX_SIZE.value)]]  # For read ops
+    write_data: Optional[Annotated[str, Field(min_length=1, max_length=ServerConfig.CHUNK_MAX_SIZE.value, frozen=True)]]    # For write ops
+    
+    # Attributes exclusive to file reads
+    return_partial: Optional[Annotated[bool, Field(default=True)]]
+    cursor_keepalive: Optional[Annotated[bool, Field(default=False)]]
 
     @model_validator(mode='after')
     def file_op_semantic_check(self) -> 'BaseFileComponent':
@@ -33,17 +40,40 @@ class BaseFileComponent(BaseModel):
         return self
 
 class BasePermissionComponent(BaseModel):
-    subject_file: Annotated[str, Field(frozen=True)]
-    subject_user: Optional[str]
-    effect_duration: Optional[Annotated[int, Field(le=0, ge=2_678_400, frozen=True, default=0)]]
+    # Request subjects
+    subject_file: Annotated[str, Field(frozen=True, pattern=ServerConfig.FILENAME_REGEX.value)]
+    subject_user: Optional[Annotated[Union[str, Literal['*']], Field(frozen=True, default=None, pattern=ServerConfig.USERNAME_REGEX.value)]] # + For grnting, - for removal
     
+    # Permission data
+    permission_flags: Annotated[PermissionFlag, Field(frozen=True)]
+    effect_duration: Optional[Annotated[int, Field(le=ServerConfig.EFFECT_DURATION_RANGE.value[0], ge=ServerConfig.EFFECT_DURATION_RANGE.value[1], frozen=True, default=0)]]
+
+    @model_validator(mode='after')
+    def permission_logic_check(self) -> 'BasePermissionComponent':
+        if self.permission_flags & (PermissionFlag.OWN.value | PermissionFlag.GRANT.value):   # Grant and ownership permissions are high level and cannot be given globally at once
+            if self.subject_user == '*':
+                # Attempt to make every user the owner/permission granter of this file
+                raise ValueError('Attempt to make ownership global')
+            if self.effect_duration:
+                # Attempt to transfer ownership or grant access temporarily
+                raise ValueError('Attempt to set duration to owner/grant permission')
+        return self
+
 class BaseHeaderComponent(BaseModel):
-    version: Annotated[int, Field(ge=0)]
-    auth_size: Annotated[int, Field(frozen=True)]
-    body_size: Annotated[int, Field(frozen=True)]
-    sender_address: Annotated[IPvAnyAddress, Field(frozen=True)]
-    sequence_number: Annotated[int, Field(frozen=True, ge=1)]
-    sender_timestamp: datetime
-    finish: bool = False
+    version: Annotated[str, Field(min_length=6, max_length=12, pattern=r'[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}')]
+
+    # Read ahead logic
+    auth_size: Annotated[int, Field(frozen=True, default=0)]
+    body_size: Annotated[int, Field(frozen=True, default=0)]
+
+    # Sender metadata
+    sender_hostname: Annotated[IPvAnyAddress, Field(frozen=True)]
+    sender_port: Annotated[int, Field(frozen=True, max_digits=5)]
+    sender_timestamp: Annotated[float, Field(frozen=True)]
+
+    # Connection status
+    finish = Annotated[bool, Field(frozen=True, default=True)]
+
+    # Message category
     category: Annotated[CategoryFlag, Field(frozen=True)]    # 0b0001, 0b0010, 0b0100, and 0b1000
-    subcategory: Annotated[int, Field(frozen=True, ge=1)]   # Also bitmask literals, but depending on parent category the values can differ
+    subcategory: Annotated[int, Field(frozen=True, ge=1)]   # Also bitmask literals, but depending on parent category the number of values can differ
