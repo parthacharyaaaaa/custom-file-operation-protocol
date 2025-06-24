1f0b0dcec25cec5c88c41106dc07d8eb086d670d
Parth Acharya <1008parth@gmail.com>
2025-06-24 14:42:42 +0530

    Removes dataclasses from error classes

--- server/errors.py
+++ server/errors.py
@@ -1,35 +1,32 @@
 from abc import ABC
-from dataclasses import dataclass, field
 from datetime import datetime
 
-@dataclass(frozen=True, slots=True)
 class ProtocolException(ABC, BaseException):
     '''Abstract base exception class for all protocol specific exceptions. Contains all the bare minimum data required to construct and send a response to an erroneous request'''
     code: str
     description: str
-    exception_iso_timestamp: str = field(default_factory=datetime.now().isoformat())
+    exception_iso_timestamp: datetime
 
-@dataclass(frozen=True, slots=True)
+    def __init__(self, description: str):
+        self.description = description
+        self.exception_iso_timestamp = datetime.now().isoformat()
+
 class SlowStreamRate(ProtocolException):
     code: str = '2:us'
     description: str = 'Stream rate too slow, ensure proper network connection'
 
-@dataclass(frozen=True, slots=True)
 class InvalidHeaderSemantic(ProtocolException):
     code: str = '2:ihs'
     description: str = 'Header semantics incorrect, please ensure that all necessary fields are present'
 
-@dataclass(frozen=True, slots=True)
 class InvalidHeaderValues(ProtocolException):
     code: str = '2:ihv'
     description: str = 'Header values incorrect'
 
-@dataclass(frozen=True, slots=True)
 class InvalidAuthSemantic(ProtocolException):
     code: str = '2:ias'
     description: str = 'Auth semantics incorrect, please ensure that all necessary fields are present'
 
-@dataclass(frozen=True, slots=True)
 class InvalidAuthData(ProtocolException):
     code: str = '2:iad'
     description: str = 'Auth values incorrect'
