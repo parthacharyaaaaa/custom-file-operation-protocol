7d0539eb16e59139b21ac23da1e797bd3c8ffc1f
Parth Acharya <1008parth@gmail.com>
2025-06-24 13:24:40 +0530

    Adds basic outline for SessionMaster class

---
+++ server/authz/session_master.py
@@ -0,0 +1,24 @@
+import os
+from server.authz.singleton import MetaSessionMaster
+
+class SessionMaster(metaclass=MetaSessionMaster):
+    '''Class for managing user sessions'''
+
+    def __init__(self):
+        self.PID
+        pass
+
+    def authorize_session():
+        pass
+
+    def authenticate_session():
+        pass
+
+    def terminate_session():
+        pass
+
+    def refresh_session():
+        pass
+
+    def ban():
+        pass
