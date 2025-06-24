a9c5900c04db5daf1f58a37c122896d059bb1d5b
Parth Acharya <1008parth@gmail.com>
2025-06-25 00:00:39 +0530

    Updates ServerConfig enum, adds IntFlag enum: PermissionFlag

--- server/config.py
+++ server/config.py
@@ -6,12 +6,19 @@ class CategoryFlag(IntFlag):
     FILE_OP = 0b0100
     PERMISSION = 0b1000
 
+class PermissionFlag(IntFlag):
+    READ = 0b00001
+    WRITE = 0b00010
+    DELETE = 0b00100
+    GRANT = 0b01000
+    OWN = 0b10000
+
 class ServerConfig(Enum):
     # Address
     HOST: str = '127.0.0.1'
     PORT: int = 6090
 
-    VERSION: int = 1
+    VERSION: str = '0.0.1'
     HEADER_READ_TIMEOUT: float = 1.0
     HEADER_READ_BYTESIZE: int = 200
 
@@ -29,14 +36,21 @@ class ServerConfig(Enum):
 
     # File I/O
     FILE_CONTENTION_TIMEOUT: float = 5
+    FILENAME_REGEX: str = r'^(?!.*[\\/])(?=.{1,128}\.[a-zA-Z0-9]{2,10}$)[^\\/]+$'
     FILE_CACHE_SIZE: int = 500
     FILE_CACHE_TTL: int = 180
     USER_MAX_FILES: int = 30
     CACHE_PUBLICISED_FILES: bool = True
+    CHUNK_MAX_SIZE: int = 4096
 
     # Auth
     MAX_AUTH_ATTEMPTS: int = 5
     AUTH_LOCK_TIMEOUTS: tuple[int, int, int, int] = (3600, 7200, 42600, 8400)
-    PASSWORD_RANGE: tuple[int, int] = (8, 128)
+    PASSWORD_RANGE: tuple[int, int] = (8, 256)
     USERNAME_RANGE: tuple[int, int] = (4, 64)
+    USERNAME_REGEX: str = r'^[\w](?:[\w\-]*[\w])?$'
     SESSION_LIFESPAN: int = 10800
+    REFRESH_DIGEST_LENGTH = 128
+
+    # Permissions
+    EFFECT_DURATION_RANGE: tuple[int, int] = (0, 2_678_400)
