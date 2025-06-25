2c3818e98be96307935abeef98260d9e07ac4df6
Parth Acharya <1008parth@gmail.com>
2025-06-25 14:42:13 +0530

    Reworks send_heartbeat to use proper header component

--- server/comms_utils/outgoing.py
+++ server/comms_utils/outgoing.py
@@ -1,10 +1,12 @@
 '''Outgoing messages from server to client'''
 import asyncio
 from server.models.request_model import BaseHeaderComponent
+from server.models.response_models import ResponseHeader
+import orjson
 
 async def send_heartbeat(header: BaseHeaderComponent, writer: asyncio.StreamWriter, close_conn: bool = False) -> None:
     '''Send a heartbeat signal back to the client'''
-    writer.write(b'{"heartbeat" : "doki doki"}\n')
+    writer.write(orjson.dumps(ResponseHeader.make_response_header(header.version, '1:hb', description="Doki Doki").model_dump_json()))
     await writer.drain()
     if close_conn:
         writer.write_eof()
