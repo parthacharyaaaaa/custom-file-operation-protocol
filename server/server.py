acea1fcab146fbb2a8bc728ddd1391466cc66ed7
Parth Acharya <1008parth@gmail.com>
2025-06-26 12:57:33 +0530

    Updates client connection callback to handle unchecked errors

--- server/server.py
+++ server/server.py
@@ -9,33 +9,41 @@ from server.models.response_models import ResponseHeader
 from server.comms_utils.incoming import process_header
 from server.comms_utils.outgoing import send_heartbeat
 from server.dispatch import TOP_LEVEL_REQUEST_MAPPING
-from server.errors import UnsupportedOperation
+from server.errors import UnsupportedOperation, InternalServerError
 import orjson
 
 async def callback(reader: asyncio.StreamReader, writer: asyncio.StreamWriter, session_master: SessionMaster) -> None:
-    header_component: BaseHeaderComponent = await process_header(ServerConfig.HEADER_READ_BYTESIZE.value, reader, writer)
-    if not header_component:
-        return
-    
-    # TODO: Add early on logic to terminate user sessions on header_component.finish being True
-    # On heartbeat signal, return early
-    if header_component.category == CategoryFlag.HEARTBEAT:
-        return await send_heartbeat(header_component, writer, close_conn=header_component.finish)
-    
-    handler: FunctionType = TOP_LEVEL_REQUEST_MAPPING.get(header_component.category)
-    if not handler:
-        writer.write(orjson.dumps(
-            ResponseHeader
-            .from_protocol_exception(UnsupportedOperation, context_request=header_component, end_conn=header_component.finish)
-            .model_dump_json()))
-        await writer.drain()
-        if header_component.finish:
-            writer.write_eof()
+    try:
+        header_component: BaseHeaderComponent = await process_header(ServerConfig.HEADER_READ_BYTESIZE.value, reader, writer)
+        if not header_component:
+            return
+        
+        # TODO: Add early on logic to terminate user sessions on header_component.finish being True
+        # On heartbeat signal, return early
+        if header_component.category == CategoryFlag.HEARTBEAT:
+            return await send_heartbeat(header_component, writer, close_conn=header_component.finish)
+        
+        handler: FunctionType = TOP_LEVEL_REQUEST_MAPPING.get(header_component.category)
+        if not handler:
+            writer.write(orjson.dumps(
+                ResponseHeader
+                .from_protocol_exception(UnsupportedOperation, context_request=header_component, end_conn=header_component.finish)
+                .model_dump_json()))
             await writer.drain()
-            writer.close()
-            return await writer.wait_closed()
-    
-    await handler()
+            if header_component.finish:
+                writer.write_eof()
+                await writer.drain()
+                writer.close()
+                return await writer.wait_closed()
+        
+        await handler()
+    except:
+        # Unhandled exceptions
+        writer.write(orjson.dumps(
+            ResponseHeader.from_unverifiable_data(InternalServerError)
+            .model_dump_json()
+        ))
+        return await writer.drain()
 
 async def main() -> None:
     if not response_codes:
