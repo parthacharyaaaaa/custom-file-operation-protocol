badc8bd21b7a6b34d08eec097e115df1e725255a
Parth Acharya <1008parth@gmail.com>
2025-07-02 00:28:03 +0530

    Updates env loading fpath for server package

--- server/__init__.py
+++ server/__init__.py
@@ -4,7 +4,7 @@ import yaml
 from types import MappingProxyType
 from dotenv import load_dotenv
 
-_loaded: bool = load_dotenv(os.path.join(__package__, 'server.env'))
+_loaded: bool = load_dotenv(os.path.join(__package__, '.env'))
 if not _loaded:
     raise RuntimeError('Failed to load env vars')
 
