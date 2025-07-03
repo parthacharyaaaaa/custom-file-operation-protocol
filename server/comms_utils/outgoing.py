03f9a98735fdb9495c74567f56131bc208893715
Parth Acharya <1008parth@gmail.com>
2025-07-03 17:58:50 +0530

    Adds generic send_response function

--- server/comms_utils/outgoing.py
+++ server/comms_utils/outgoing.py
@@ -1,8 +1,9 @@
 '''Outgoing messages from server to client'''
 import asyncio
-from server.models.request_model import BaseHeaderComponent
-from server.models.response_models import ResponseHeader
 import orjson
+from server.models.request_model import BaseHeaderComponent
+from server.models.response_models import ResponseHeader, ResponseBody
+from typing import Optional
 
 async def send_heartbeat(header: BaseHeaderComponent, writer: asyncio.StreamWriter, close_conn: bool = False) -> None:
     '''Send a heartbeat signal back to the client'''
@@ -13,3 +14,14 @@ async def send_heartbeat(header: BaseHeaderComponent, writer: asyncio.StreamWrit
         writer.close()
         await writer.wait_closed()
     return
+
+async def send_response(writer: asyncio.StreamWriter, response: ResponseHeader, body: Optional[ResponseBody] = None, close_conn: bool = False) -> None:
+    writer.write(orjson.dumps(response.model_dump()))
+    if body:
+        writer.write(orjson.dump(body.model_dump()))
+    await writer.drain()
+    if close_conn:
+        writer.write_eof()
+        await writer.drain()
+        writer.close()
+        await writer.wait_closed()
