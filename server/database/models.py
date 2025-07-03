c66ab0ee69c50ae76074385d3b5d16bf67a8fd67
Parth Acharya <1008parth@gmail.com>
2025-07-03 14:18:45 +0530

    Adds Pydantic model for ACTIVITY_LOGS class

---
+++ server/database/models.py
@@ -0,0 +1,13 @@
+from pydantic import BaseModel, Field, IPvAnyAddress
+from typing import Literal, Annotated, Optional
+from datetime import datetime
+
+class ActivityLog(BaseModel):
+    '''Pydantic object mapping to relation ACTIVITY_LOGS'''
+    occurance_time: Annotated[float, Field(frozen=True, default_factory=datetime.now)]
+    severity: Annotated[int, Field(le=1, ge=5, default=1)]
+    logged_by: Annotated[Literal['session_master', 'connection_master', 'file_handler', 'socket_handler', 'bootup_handler', 'permission_handler', 'stream_parser', 'admin', 'cronjob'], Field(default='internal')]
+    log_type: Annotated[Literal['user', 'database', 'session', 'request', 'network', 'internal', 'permission', 'audit', 'unknown'], Field(default='unknown')]
+    log_details: Optional[Annotated[str, Field(max_length=512)]]
+    user_concerned: Optional[Annotated[str, Field(max_length=128)]]
+    host_concerned: Annotated[IPvAnyAddress, Field(frozen=True)]
