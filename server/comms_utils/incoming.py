5831ebabc3a0b763b833bbdc0f00651c6b5ae997
Parth Acharya <1008parth@gmail.com>
2025-07-04 17:35:17 +0530

    Updates server/comms_utils/incoming to have a single generic function for reading, parsing, and validating componentsfrom stream

--- server/comms_utils/incoming.py
+++ server/comms_utils/incoming.py
@@ -3,22 +3,28 @@ import asyncio
 from pydantic import ValidationError
 import server.errors as exc
 from server.config import ServerConfig
-import server.models.request_model as req_models
+from server.models.request_model import BaseHeaderComponent, BaseAuthComponent, BaseFileComponent, BasePermissionComponent, RequestComponentType
 from server.parsers import serialize_json
-from typing import Any, Optional
+from typing import Any, Literal
 import orjson
 
-async def process_header(n_bytes: int, reader: asyncio.StreamReader) -> Optional[req_models.BaseHeaderComponent]:
+async def process_component(n_bytes: int, reader: asyncio.StreamReader, component_type: Literal['header', 'auth', 'file', 'permission']) -> RequestComponentType:
+    model, timeout = None, None
+    if component_type == 'header':
+        model, timeout = BaseHeaderComponent, ServerConfig.HEADER_READ_TIMEOUT.value
+    elif component_type == 'auth':
+        model, timeout = BaseAuthComponent, ServerConfig.AUTH_READ_TIMEOUT.value
+    elif component_type == 'file':
+        model, timeout = BaseFileComponent, ServerConfig.FILE_READ_TIMEOUT.value
+    elif component_type == 'permission':
+        model, timeout = BasePermissionComponent, ServerConfig.PERMISSION_READ_TIMEOUT.value
+    
     try:
-        raw_header: bytes = await asyncio.wait_for(reader.readexactly(n_bytes), timeout=ServerConfig.HEADER_READ_TIMEOUT.value)
-        header: dict[str, Any] = await serialize_json(raw_header)
-        return req_models.BaseHeaderComponent.model_validate(obj=header)
+        raw_component: bytes = await asyncio.wait_for(reader.readexactly(n_bytes), timeout)
+        component_mapping: dict[str, Any] = await serialize_json(raw_component)
+        return model.model_validate_json(component_mapping)
+    
     except (asyncio.IncompleteReadError, ValidationError, orjson.JSONDecodeError):
         raise exc.InvalidHeaderSemantic
     except asyncio.TimeoutError:
         raise exc.SlowStreamRate
-    
-async def process_auth(n_bytes: int, reader: asyncio.StreamReader) -> req_models.BaseAuthComponent:
-    raw_auth: bytes = await asyncio.wait_for(reader.readexactly(n_bytes), timeout=ServerConfig.AUTH_READ_TIMEOUT.value)
-    auth_mapping: dict[str, Any] = await serialize_json(raw_auth)
-    return req_models.BaseAuthComponent.model_validate_json(auth_mapping)
