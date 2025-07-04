159314b9d546cd254539f1ebef7f103d1b270d74
Parth Acharya <1008parth@gmail.com>
2025-07-04 14:06:16 +0530

    Adds infinite loop to callback function to allow repeated duplex communication

--- server/server.py
+++ server/server.py
@@ -14,29 +14,40 @@ from server.models.request_model import BaseHeaderComponent
 from server.models.response_models import ResponseHeader, ResponseBody
 
 async def callback(reader: asyncio.StreamReader, writer: asyncio.StreamWriter) -> None:
-    try:
-        header_component: BaseHeaderComponent = await process_header(ServerConfig.HEADER_READ_BYTESIZE.value, reader, writer)
-        if not header_component:
-            raise SlowStreamRate('Unable to parse header')
-        
-        handler: Callable[..., Coroutine[Any, Any, tuple[ResponseHeader, Optional[ResponseBody]]]] = TOP_LEVEL_REQUEST_MAPPING.get(header_component.category)
-        if not handler:
-            raise UnsupportedOperation(f'Operation category must be in: {", ".join(CategoryFlag._member_names_)}')
-        
-        response_header, response_body = await handler(header_component)
-        body_stream: bytes = None
-        if response_body:
-            body_stream: bytes = orjson.dumps(response_body.model_dump())
-            response_header.body_size = len(body_stream)
-        
-        return await send_response(writer=writer, response=response_header, body=body_stream, connection_end=response_header.ended_connection)
+    while not reader.at_eof():
+        try:
+            header_component: BaseHeaderComponent = await process_header(ServerConfig.HEADER_READ_BYTESIZE.value, reader, writer)
+            if not header_component:
+                raise SlowStreamRate('Unable to parse header')
+            
+            handler: Callable[[BaseHeaderComponent], Coroutine[Any, Any, tuple[ResponseHeader, Optional[ResponseBody]]]] = TOP_LEVEL_REQUEST_MAPPING.get(header_component.category)
+            if not handler:
+                raise UnsupportedOperation(f'Operation category must be in: {", ".join(CategoryFlag._member_names_)}')
+            
+            response_header, response_body = await handler(header_component)
+            body_stream: bytes = None
+            if response_body:
+                body_stream: bytes = orjson.dumps(response_body.model_dump())
+                response_header.body_size = len(body_stream)
+            
+            await send_response(writer=writer, response=response_header, body=body_stream)
+            
+            if response_header.ended_connection or header_component.connection_keepalive:
+                writer.close()
+                await writer.wait_closed()
+                return
 
-    except Exception as e:
-        connection_end: bool = False if not header_component else header_component.finish
-        response: ResponseHeader = ResponseHeader.from_protocol_exception(exc=e if issubclass(e, ProtocolException) else InternalServerError,
-                                                                          context_request=header_component,
-                                                                          end_conn=connection_end)
-        return await send_response(writer=writer, response=response, close_conn=connection_end)
+        except Exception as e:
+            connection_end: bool = False if not header_component else header_component.finish
+            response: ResponseHeader = ResponseHeader.from_protocol_exception(exc=e if isinstance(e, ProtocolException) else InternalServerError,
+                                                                              context_request=header_component,
+                                                                              end_conn=connection_end)
+            await send_response(writer=writer, response=response)
+            if connection_end:
+                writer.close()
+                await writer.wait_closed()
+                return
+            
 
 async def main() -> None:    
     # Initialize all extensions that the server depends on
