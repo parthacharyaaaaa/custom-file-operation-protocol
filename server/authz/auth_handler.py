de7549167d23b4aace7ac6f90f86b28fd0f85c68
Parth Acharya <1008parth@gmail.com>
2025-07-04 17:37:12 +0530

    Swaps out process_auth with process_component, removes redundant exception handling

--- server/authz/auth_handler.py
+++ server/authz/auth_handler.py
@@ -1,15 +1,13 @@
 import asyncio
-import orjson
 from server.authz.auth_flags import AuthFlags
 from server.authz.auth_operations import handle_registration, handle_login, handle_session_refresh, handle_password_change, handle_deletion, handle_session_termination
 from server.config import CategoryFlag
-from server.comms_utils.incoming import process_auth
-from server.errors import InvalidAuthSemantic, SlowStreamRate, UnsupportedOperation
+from server.comms_utils.incoming import process_component
+from server.errors import InvalidAuthSemantic, UnsupportedOperation
 from server.models.request_model import BaseHeaderComponent, BaseAuthComponent
 from server.models.response_models import ResponseHeader, ResponseBody
 from typing import Optional, Coroutine, Any, Callable, TypeAlias
 from types import MappingProxyType
-from pydantic import ValidationError
 
 AUTH_SUBHABDLER: TypeAlias = Callable[[BaseHeaderComponent, BaseAuthComponent],
                                       Coroutine[Any, Any, tuple[ResponseHeader, Optional[ResponseBody]]]]
@@ -23,17 +21,11 @@ _AUTH_SUBHANDLER_MAPPING: MappingProxyType[int, AUTH_SUBHABDLER] = MappingProxyT
     )
 )
 
-async def top_auth_handler(reader: asyncio.StreamReader, writer: asyncio.StreamWriter, header_component: BaseHeaderComponent) -> tuple[ResponseHeader, Optional[ResponseBody]]:
+async def top_auth_handler(reader: asyncio.StreamReader, header_component: BaseHeaderComponent) -> tuple[ResponseHeader, Optional[ResponseBody]]:
     if not header_component.auth_size:
         raise InvalidAuthSemantic('Missing auth component in header, and no unauthenticated operation requested')
 
-    # Auth component exists
-    try:
-        auth_component: BaseAuthComponent = await process_auth(header_component.auth_size, reader, writer)
-    except asyncio.TimeoutError:
-        raise SlowStreamRate
-    except (asyncio.IncompleteReadError, ValidationError, orjson.JSONDecodeError):
-        raise InvalidAuthSemantic
+    auth_component: BaseAuthComponent = await process_component(n_bytes=header_component.auth_size, reader=reader, component_type='auth')
 
     if header_component.subcategory not in AuthFlags._value2member_map_:    # R level function name, absolutely vile.
         raise UnsupportedOperation(f'Unsupported operation for category: {CategoryFlag.AUTH._name_}')
