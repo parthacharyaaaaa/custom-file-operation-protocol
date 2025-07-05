8716633db08da593ba7790c8e62d5fa64dcafee9
Parth Acharya <1008parth@gmail.com>
2025-07-05 13:51:20 +0530

    Minor fixes

--- server/server.py
+++ server/server.py
@@ -5,7 +5,7 @@ from functools import partial
 from typing import Optional, Any, Coroutine, Callable
 from psycopg.conninfo import make_conninfo
 from server.bootup import init_connection_master, init_user_master, init_file_lock
-from server.comms_utils.incoming import process_header
+from server.comms_utils.incoming import process_component
 from server.comms_utils.outgoing import send_response
 from server.config import ServerConfig, CategoryFlag
 from server.dispatch import TOP_LEVEL_REQUEST_MAPPING
@@ -16,7 +16,9 @@ from models.response_models import ResponseHeader, ResponseBody
 async def callback(reader: asyncio.StreamReader, writer: asyncio.StreamWriter) -> None:
     while not reader.at_eof():
         try:
-            header_component: BaseHeaderComponent = await process_header(ServerConfig.HEADER_READ_BYTESIZE.value, reader)
+            header_component: BaseHeaderComponent = await process_component(n_bytes=ServerConfig.HEADER_READ_BYTESIZE.value,
+                                                                            reader=reader,
+                                                                            component_type='header')
             if not header_component:
                 raise SlowStreamRate('Unable to parse header')
             
@@ -52,9 +54,8 @@ async def callback(reader: asyncio.StreamReader, writer: asyncio.StreamWriter) -
 async def main() -> None:    
     # Initialize all extensions that the server depends on
     init_user_master(ServerConfig)
-    init_connection_master(
-        make_conninfo(user=os.environ['PG_USERNAME'], password=os.environ['PG_PASSWORD'],
-                      host=os.environ['PG_HOST'], port=os.environ['PG_PORT'], dbname=os.environ['PG_DBNAME']))
+    init_connection_master(conninfo=make_conninfo(user=os.environ['PG_USERNAME'], password=os.environ['PG_PASSWORD'], host=os.environ['PG_HOST'], port=os.environ['PG_PORT'], dbname=os.environ['PG_DBNAME']),
+                           config=ServerConfig)
     init_file_lock()
 
     # Start server
