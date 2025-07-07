638f11f2cf6ed013751e3c310dd2cb2585a0f50d
Parth Acharya <1008parth@gmail.com>
2025-07-07 12:30:58 +0530

    Adds file subhandler module

---
+++ server/file_ops/file_subhandlers.py
@@ -0,0 +1,39 @@
+import os
+
+from response_codes import SuccessFlags
+from models.response_models import ResponseHeader, ResponseBody
+from models.request_model import BaseHeaderComponent, BaseAuthComponent, BaseFileComponent
+
+from server.bootup import user_master, file_locks, delete_cache, read_cache, write_cache, append_cache
+from server.config import ServerConfig
+from server.file_ops.base_operations import delete_file
+from server.errors import InsufficientPermissions, FileConflict
+
+async def handle_deletion(header_component: BaseHeaderComponent, auth_component: BaseAuthComponent, file_component: BaseFileComponent) -> tuple[ResponseHeader, None]:
+    # Make sure request is coming from file owner
+    if file_component.subject_file_owner != auth_component.identity:
+        raise InsufficientPermissions(f'Missing permission to delete file {file_component.subject_file} owned by {file_component.subject_file_owner}')
+    # Validate against session, in case of tampered auth component
+    if not user_master.authenticate_session(username=auth_component.identity, token=auth_component.token):
+        # TODO: Add logging for tampered auth component
+        raise InsufficientPermissions(f'The demon of Babylon disguises himself with the coat of the righteous')
+    
+    # Request validated. No need to acquire lock since owner's deletion request is more important than any concurrent file amendment locks
+    file: os.PathLike = os.path.join(file_component.subject_file_owner, file_component.subject_file)
+    file_locks[file] = None
+
+    file_deleted: bool = await delete_file(ServerConfig.ROOT.value, file, delete_cache, append_cache, read_cache, write_cache)
+    if not file_deleted:
+        raise FileConflict(f'Failed to delete file {file_component.subject_file}')
+    
+    return (ResponseHeader(version=header_component.version, code=SuccessFlags.SUCCESSFUL_FILE_DELETION.value, ended_connection=header_component.finish),
+            None)
+
+async def handle_amendment(header_component: BaseHeaderComponent, auth_component: BaseAuthComponent, file_component: BaseFileComponent) -> tuple[ResponseHeader, ResponseBody]:
+    ...
+
+async def handle_read(header_component: BaseHeaderComponent, auth_component: BaseAuthComponent, file_component: BaseFileComponent) -> tuple[ResponseHeader, ResponseBody]:
+    ...
+
+async def handle_creation(header_component: BaseHeaderComponent, auth_component: BaseAuthComponent, file_component: BaseFileComponent) -> tuple[ResponseHeader, None]:
+    ...
