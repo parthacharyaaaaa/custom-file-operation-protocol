dd031c15eb4350760e80d16dcf07cf5375df8b98
Parth Acharya <1008parth@gmail.com>
2025-07-07 12:54:27 +0530

    Adds handle_read() subhandler for file I/o

--- server/file_ops/file_subhandlers.py
+++ server/file_ops/file_subhandlers.py
@@ -6,8 +6,12 @@ from models.request_model import BaseHeaderComponent, BaseAuthComponent, BaseFil
 
 from server.bootup import user_master, file_locks, delete_cache, read_cache, write_cache, append_cache
 from server.config import ServerConfig
-from server.file_ops.base_operations import delete_file
+from server.file_ops.base_operations import read_file, delete_file
 from server.errors import InsufficientPermissions, FileConflict
+from server.permission_ops.permission_operations import check_file_permission
+from server.database.models import role_types
+
+import orjson
 
 async def handle_deletion(header_component: BaseHeaderComponent, auth_component: BaseAuthComponent, file_component: BaseFileComponent) -> tuple[ResponseHeader, None]:
     # Make sure request is coming from file owner
@@ -33,7 +37,22 @@ async def handle_amendment(header_component: BaseHeaderComponent, auth_component
     ...
 
 async def handle_read(header_component: BaseHeaderComponent, auth_component: BaseAuthComponent, file_component: BaseFileComponent) -> tuple[ResponseHeader, ResponseBody]:
-    ...
+    # Authetenticate session
+    if not user_master.authenticate_session(username=auth_component.identity, token=auth_component.token):
+        raise InsufficientPermissions(f'Invalid session and/or auth component for user {auth_component.identity}')
+    
+    # Check permissions
+    if not check_file_permission(filename=file_component.subject_file, owner=file_component.subject_file_owner, grantee=auth_component.identity, check_for=role_types):
+        raise InsufficientPermissions(f'User {auth_component.identity} does not have read permission on file {file_component.subject_file} owned by {file_component.subject_file_owner}')
+    
+    fpath: os.PathLike = os.path.join(file_component.subject_file_owner, file_component.subject_file)
+    read_data, cursor_position, eof_reached = await read_file(root=ServerConfig.ROOT.value, fpath=fpath,
+                                                              deleted_cache=delete_cache, read_cache=read_cache,
+                                                              cursor_position=file_component.cursor_position, nbytes=file_component.chunk_size, reader_keepalive=file_component.cursor_keepalive,
+                                                              purge_reader=header_component.finish, identifier=auth_component.identity, cached=True)
+    
+    return (ResponseHeader(version=header_component.version, code=SuccessFlags.SUCCESSFUL_READ, ended_connection=header_component.finish),
+            ResponseBody(contents=orjson.dumps({'read' : read_data}), return_partial=not eof_reached, chunk_number=file_component.chunk_number+1, keepalive_accepted=bool(read_cache.get(fpath)))) 
 
 async def handle_creation(header_component: BaseHeaderComponent, auth_component: BaseAuthComponent, file_component: BaseFileComponent) -> tuple[ResponseHeader, None]:
     ...
