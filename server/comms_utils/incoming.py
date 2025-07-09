f5b4b2a9e2ad3e686b65ae57499913efefa3fa4d
Parth Acharya <1008parth@gmail.com>
2025-07-09 12:11:52 +0530

    Moves parsing logic to /server/comms_utils/incoming.py

--- server/comms_utils/incoming.py
+++ server/comms_utils/incoming.py
@@ -1,12 +1,31 @@
 '''Utils for incoming streams from client to server'''
 import asyncio
-from pydantic import ValidationError
-import server.errors as exc
-from server.config import ServerConfig
-from models.request_model import BaseHeaderComponent, BaseAuthComponent, BaseFileComponent, BasePermissionComponent, RequestComponentType
-from server.parsers import serialize_json
 from typing import Any, Literal
+from types import MappingProxyType
+
+from models.request_model import BaseHeaderComponent, BaseAuthComponent, BaseFileComponent, BasePermissionComponent, RequestComponentType
+from models.flags import CategoryFlag
+
+from server.config import ServerConfig
+import server.errors as exc
+
+from pydantic import BaseModel, ValidationError
 import orjson
+
+CATEGORY_MODEL_MAP: MappingProxyType[int, type[BaseModel]] = MappingProxyType({CategoryFlag.AUTH: BaseAuthComponent, CategoryFlag.FILE_OP: BaseFileComponent, CategoryFlag.PERMISSION: BasePermissionComponent})
+
+async def serialize_json(data: bytes, awaitable_lower_bound: int = 2048, await_timeout: float = 5) -> dict[str, Any]:
+    if len(data) < awaitable_lower_bound:
+        return orjson.loads(data)
+    return await asyncio.wait_for(asyncio.to_thread(orjson.loads(data)), timeout=await_timeout)
+
+async def parse_body(header: BaseHeaderComponent, body: bytes) -> BaseModel:
+    component_cls: type[BaseModel] = CATEGORY_MODEL_MAP.get(header.category)
+    if not component_cls:
+        raise ValueError('Unsupported category')
+    
+    body_mapping: dict[str, Any] = await serialize_json(body)
+    return component_cls.model_validate(body_mapping)
 
 async def process_component(n_bytes: int, reader: asyncio.StreamReader, component_type: Literal['header', 'auth', 'file', 'permission']) -> RequestComponentType:
     model, timeout = None, None
