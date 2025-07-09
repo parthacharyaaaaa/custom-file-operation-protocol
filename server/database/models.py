737ee2bd3924dd2c0bae3ff090e63343a473b62b
Parth Acharya <1008parth@gmail.com>
2025-07-09 15:20:52 +0530

    Updates ActivityLog model

--- server/database/models.py
+++ server/database/models.py
@@ -6,10 +6,10 @@ role_types: TypeAlias = Literal['owner', 'manager', 'reader', 'editor']
 
 class ActivityLog(BaseModel):
     '''Pydantic object mapping to relation ACTIVITY_LOGS'''
-    occurance_time: Annotated[float, Field(frozen=True, default_factory=datetime.now)]
-    severity: Annotated[int, Field(le=1, ge=5, default=1)]
-    logged_by: Annotated[Literal['user_master', 'connection_master', 'file_handler', 'socket_handler', 'bootup_handler', 'permission_handler', 'stream_parser', 'admin', 'cronjob'], Field(default='internal')]
-    log_type: Annotated[Literal['user', 'database', 'session', 'request', 'network', 'internal', 'permission', 'audit', 'unknown'], Field(default='unknown')]
-    log_details: Optional[Annotated[str, Field(max_length=512)]]
-    user_concerned: Optional[Annotated[str, Field(max_length=128)]]
-    host_concerned: Optional[Annotated[IPvAnyAddress, Field(frozen=True)]]
+    occurance_time: Annotated[datetime, Field(frozen=True, default_factory=datetime.now)]
+    severity: Annotated[int, Field(le=5, ge=1, default=1)]
+    logged_by: Annotated[Literal['user_master', 'connection_master', 'file_handler', 'socket_handler', 'bootup_handler', 'permission_handler', 'stream_parser', 'admin', 'cronjob'], Field(default='connection_master')]
+    log_category: Annotated[Literal['user', 'database', 'session', 'request', 'network', 'internal', 'permission', 'audit', 'unknown'], Field(default='unknown')]
+    log_details: Annotated[Optional[str], Field(max_length=512, default=None)]
+    user_concerned: Annotated[Optional[str], Field(max_length=128, default=None)]
+    host_concerned: Annotated[Optional[IPvAnyAddress], Field(frozen=True, default=None)]
