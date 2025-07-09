f93c08c456f2b17ab551ca99bcd6ae1b1ec2eca0
Parth Acharya <1008parth@gmail.com>
2025-07-09 15:24:11 +0530

    Fixes invalid placeholder interpolation

--- server/logging.py
+++ server/logging.py
@@ -11,7 +11,7 @@ LOG_INSERTION_SQL: sql.SQL = (sql.SQL('''INSERT INTO {tablename} ({columns_templ
                                         VALUES ({placeholder_template});''')
                                         .format(tablename=sql.Identifier('activity_logs'),
                                                 columns_template=sql.SQL(', ').join([sql.Identifier(key) for key in list(ActivityLog.model_fields.keys())]),
-                                                placeholder_template=sql.SQL(', ').join(['%s' for _ in range(len(ActivityLog.model_fields))])))
+                                                placeholder_template=sql.SQL(', ').join([sql.Placeholder() for _ in range(len(ActivityLog.model_fields))])))
 
 async def flush_logs(connection_master: ConnectionPoolManager, queue: asyncio.PriorityQueue[ActivityLog], batch_size: int, waiting_period: float, flush_interval: float) -> None:
     log_entries: list[ActivityLog] = []
