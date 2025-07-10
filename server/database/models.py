7548be76ea82808671c83bb0190a76c0fc094b78
Parth Acharya <1008parth@gmail.com>
2025-07-10 12:22:59 +0530

    Formats enums

--- server/database/models.py
+++ server/database/models.py
@@ -3,6 +3,8 @@ from typing import Literal, Annotated, Optional
 from datetime import datetime
 from enum import Enum, IntFlag
 
+role_types = Literal['owner', 'manager', 'reader', 'editor']
+
 class Severity(IntFlag):
     INFO                    = 1
     TRACE                   = 2
@@ -39,8 +41,8 @@ class ActivityLog(BaseModel):
     '''Pydantic object mapping to relation ACTIVITY_LOGS'''
     occurance_time: Annotated[datetime, Field(frozen=True, default_factory=datetime.now)]
     severity: Annotated[int, Field(le=5, ge=1, default=1)]
-    logged_by: Annotated[Literal['user_master', 'connection_master', 'file_handler', 'socket_handler', 'bootup_handler', 'permission_handler', 'stream_parser', 'admin', 'cronjob'], Field(default='connection_master')]
-    log_category: Annotated[LogAuthor, Field(default='unknown')]
+    logged_by: Annotated[LogAuthor, Field(default=LogAuthor.CONNECTION_MASTER.value)]
+    log_category: Annotated[LogType, Field(default=LogType.UNKNOWN.value)]
     log_details: Annotated[Optional[LogType], Field(max_length=512, default=None)]
     user_concerned: Annotated[Optional[str], Field(max_length=128, default=None)]
     host_concerned: Annotated[Optional[IPvAnyAddress], Field(frozen=True, default=None)]
