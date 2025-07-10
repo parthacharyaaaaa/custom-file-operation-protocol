f30fcc2554e3ca3639001726c484756127e33236
Parth Acharya <1008parth@gmail.com>
2025-07-10 11:21:52 +0530

    Adds __main__ file to server package, moves main() coroutine to __main__

---
+++ server/__main__.py
@@ -0,0 +1,24 @@
+import asyncio
+import os
+import sys
+
+from server.bootup import init_connection_master, init_user_master, init_file_lock, init_logger, init_caches
+from server.config.server_config import SERVER_CONFIG
+from server.callback import callback
+from psycopg.conninfo import make_conninfo
+
+async def main() -> None:
+    await init_connection_master(conninfo=make_conninfo(user=os.environ['PG_USERNAME'], password=os.environ['PG_PASSWORD'], host=os.environ['PG_HOST'], port=os.environ['PG_PORT'], dbname=os.environ['PG_DBNAME']))
+    init_user_master()
+    init_file_lock()
+    init_caches()
+    init_logger()
+
+    server: asyncio.Server = await asyncio.start_server(client_connected_cb=callback,
+                                                        host=str(SERVER_CONFIG.host), port=SERVER_CONFIG.port)
+    async with server:
+        await server.serve_forever()
+
+if sys.platform == 'win32':
+    asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+asyncio.run(main())
--- server/callback.py
+++ server/callback.py
@@ -1,16 +1,13 @@
 import asyncio
-import os
-import sys
 import orjson
 from typing import Optional, Any, Coroutine, Callable
+from traceback import format_exc
 
 from models.flags import CategoryFlag
 from models.request_model import BaseHeaderComponent
 from models.response_models import ResponseHeader, ResponseBody
 from models.constants import REQUEST_CONSTANTS
 
-from psycopg.conninfo import make_conninfo
-from server.bootup import init_connection_master, init_user_master, init_file_lock, init_caches, init_logger
 from server.comms_utils.incoming import process_component
 from server.comms_utils.outgoing import send_response
 from server.config.server_config import SERVER_CONFIG
@@ -27,11 +24,11 @@ async def callback(reader: asyncio.StreamReader, writer: asyncio.StreamWriter) -
                                                                             timeout=SERVER_CONFIG.read_timeout)
             if not header_component:
                 raise SlowStreamRate('Unable to parse header')
-            handler: Callable[[BaseHeaderComponent], Coroutine[Any, Any, tuple[ResponseHeader, Optional[ResponseBody]]]] = TOP_LEVEL_REQUEST_MAPPING.get(header_component.category)
+            handler: Callable[[asyncio.StreamReader, BaseHeaderComponent], Coroutine[Any, Any, tuple[ResponseHeader, Optional[ResponseBody]]]] = TOP_LEVEL_REQUEST_MAPPING.get(header_component.category)
             if not handler:
                 raise UnsupportedOperation(f'Operation category must be in: {", ".join(CategoryFlag._member_names_)}')
 
-            response_header, response_body = await handler(header_component)
+            response_header, response_body = await handler(reader, header_component)
             body_stream: bytes = None
             if response_body:
                 body_stream: bytes = orjson.dumps(response_body.model_dump())
@@ -46,6 +43,7 @@ async def callback(reader: asyncio.StreamReader, writer: asyncio.StreamWriter) -
                 return
 
         except Exception as e:
+            print(format_exc())
             connection_end: bool = False if not header_component else header_component.finish
             response: ResponseHeader = ResponseHeader.from_protocol_exception(exc=e if isinstance(e, ProtocolException) else InternalServerError,
                                                                             version=config.version if not header_component else header_component.version,
@@ -59,24 +57,3 @@ async def callback(reader: asyncio.StreamReader, writer: asyncio.StreamWriter) -
                 writer.close()
                 await writer.wait_closed()
                 return
-
-
-async def main() -> None:
-    # Initialize all extensions that the server depends on
-    await init_connection_master(conninfo=make_conninfo(user=os.environ['PG_USERNAME'], password=os.environ['PG_PASSWORD'], host=os.environ['PG_HOST'], port=os.environ['PG_PORT'], dbname=os.environ['PG_DBNAME']))
-    init_user_master()
-    init_file_lock()
-    init_caches()
-    init_logger()
-
-    # Start server
-    # partial is used to pass the SERVER_CONFIG and the REQUEST_CONSTANTS model
-    server: asyncio.Server = await asyncio.start_server(client_connected_cb=callback,
-                                                        host=str(SERVER_CONFIG.host), port=SERVER_CONFIG.port)
-    async with server:
-        await server.serve_forever()
-
-if __name__ == '__main__':
-    if sys.platform == 'win32':
-        asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
-    asyncio.run(main())
