f10002711af42f6733bc11c7ceeb5a0a44909bb4
Parth Acharya <1008parth@gmail.com>
2025-07-11 10:09:08 +0530

    Adds padding to response header

--- server/comms_utils/outgoing.py
+++ server/comms_utils/outgoing.py
@@ -2,6 +2,7 @@
 import asyncio
 from models.request_model import BaseHeaderComponent
 from models.response_models import ResponseHeader, ResponseBody
+from models.constants import RESPONSE_CONSTANTS
 from typing import Optional, Union
 
 async def send_heartbeat(header: BaseHeaderComponent) -> tuple[ResponseHeader, None]:
@@ -13,6 +14,8 @@ async def send_heartbeat(header: BaseHeaderComponent) -> tuple[ResponseHeader, N
 
 async def send_response(writer: asyncio.StreamWriter, header: Union[ResponseHeader, bytes], body: Optional[Union[ResponseBody, bytes]] = None, seperator: Optional[bytes] = b'') -> None:
     header_stream: bytes = header if isinstance(header, bytes) else header.as_bytes()
+    # Header is of constant size, given by RESPONSE_CONSTANTS.header.bytesize. Smaller headers will need to be padded
+    header_stream += b' ' * (RESPONSE_CONSTANTS.header.bytesize - len(header_stream))
     writer.write(header_stream + seperator)
     if body:
         body_stream: bytes = body if isinstance(body, bytes) else body.as_bytes()
