532cae2526ad13f83574edf264ff7948e047cdcf
Parth Acharya <1008parth@gmail.com>
2025-07-13 13:58:50 +0530

    Updates send_request to await acquiring global stream lock before writing to buffer and draining

--- client/communication/outgoing.py
+++ client/communication/outgoing.py
@@ -1,6 +1,7 @@
 import asyncio
 from typing import Optional, Union
 from models.request_model import BaseHeaderComponent, BaseAuthComponent, BaseFileComponent, BasePermissionComponent
+from client.bootup import stream_lock
 
 async def send_request(writer: asyncio.StreamWriter, header_component: BaseHeaderComponent, auth_component: Optional[BaseAuthComponent] = None, body_component: Optional[Union[BaseFileComponent, BasePermissionComponent]] = None) -> None:
     auth_stream = b'' if not auth_component else auth_component.model_dump_json().encode('utf-8')
@@ -9,8 +10,9 @@ async def send_request(writer: asyncio.StreamWriter, header_component: BaseHeade
     header_component.auth_size = len(auth_stream)
     header_component.body_size = len(body_stream)
 
-    writer.write(header_component.model_dump_json().encode('utf-8'))
-    writer.write(auth_stream)
-    writer.write(body_stream)
+    async with stream_lock:
+        writer.write(header_component.model_dump_json().encode('utf-8'))
+        writer.write(auth_stream)
+        writer.write(body_stream)
 
-    await writer.drain()
+        await writer.drain()
