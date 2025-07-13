31eee5039fe3e027731386289581b9cfe2d4c65d
Parth Acharya <1008parth@gmail.com>
2025-07-13 10:38:07 +0530

    Adds module for outgoing data

---
+++ client/communication/outgoing.py
@@ -0,0 +1,16 @@
+import asyncio
+from typing import Optional, Union
+from models.request_model import BaseHeaderComponent, BaseAuthComponent, BaseFileComponent, BasePermissionComponent
+
+async def send_request(writer: asyncio.StreamWriter, header_component: BaseHeaderComponent, auth_component: Optional[BaseAuthComponent] = None, body_component: Optional[Union[BaseFileComponent, BasePermissionComponent]] = None) -> None:
+    auth_stream = b'' if not auth_component else auth_component.model_dump_json().encode('utf-8')
+    body_stream = b'' if not body_component else body_component.model_dump_json().encode('utf-8')
+
+    header_component.auth_size = len(auth_stream)
+    header_component.body_size = len(body_stream)
+
+    writer.write(header_component.model_dump_json().encode('utf-8'))
+    writer.write(auth_stream)
+    writer.write(body_stream)
+
+    await writer.drain()
