28766939eeef37d52eb6e7f9fdedad26f555e166
Parth Acharya <1008parth@gmail.com>
2025-07-13 14:39:34 +0530

    Adds append_remote_file method to append data directly to a remote file

--- client/communication/file_operations.py
+++ client/communication/file_operations.py
@@ -1,7 +1,7 @@
 import asyncio
 import aiofiles
 import os
-from typing import Optional
+from typing import Optional, Union
 
 from client.bootup import session_manager
 from client.config.constants import CLIENT_CONFIG
@@ -37,3 +37,26 @@ async def upload_remote_file(reader: asyncio.StreamReader, writer: asyncio.Strea
                 raise Exception
             
             chunk_number+=1
+
+async def append_remote_file(reader: asyncio.StreamReader, writer: asyncio.StreamWriter, write_data: Union[str, bytes, bytearray, memoryview], remote_directory: str, remote_filename, chunk_size: Optional[int] = None) -> None:
+    if isinstance(write_data, str):
+        write_data = write_data.encode('utf-8')
+    
+    write_view: memoryview = write_data if isinstance(write_data, memoryview) else memoryview(write_data)
+    view_length = len(write_view)
+
+    chunk_size = min(REQUEST_CONSTANTS.file.max_bytesize, min(view_length, chunk_size or REQUEST_CONSTANTS.file.chunk_max_size))
+    valid_responses: tuple[str] = (SuccessFlags.SUCCESSFUL_AMEND.value, IntermediaryFlags.PARTIAL_AMEND.value)
+
+    for chunk_number, offset in enumerate(range(0, view_length, chunk_size)):
+        chunk: memoryview = write_view[offset:offset+chunk_size]
+        end_reached: bool = offset + chunk_size >= view_length
+        file_component: BaseFileComponent = BaseFileComponent(subject_file=remote_filename, subject_file_owner=remote_directory,
+                                                                chunk_size=chunk_size, chunk_number=chunk_number, write_data=chunk,
+                                                                return_partial=True, cursor_keepalive=end_reached)
+
+        await send_request(writer, BaseHeaderComponent(CLIENT_CONFIG.version, finish=end_reached, category=CategoryFlag.FILE_OP, subcategory=FileFlags.APPEND), session_manager.auth_component, file_component)
+
+        response_header, response_body = await process_response(reader, writer, CLIENT_CONFIG.read_timeout)
+        if response_header.code not in valid_responses:
+            raise Exception
