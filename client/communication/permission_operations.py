f93814a22e8ad3667b53608e5ce437248328fff0
Parth Acharya <1008parth@gmail.com>
2025-07-14 21:03:28 +0530

    Renames incorrectly named PermissionComponents in permission_operations

--- client/communication/permission_operations.py
+++ client/communication/permission_operations.py
@@ -26,9 +26,9 @@ async def grant_permission(reader: asyncio.StreamReader, writer: asyncio.StreamW
     if duration:
         duration = min(REQUEST_CONSTANTS.permission.effect_duration_range[1], abs(duration))    # 0th index contains lower bound, 1st index contains upper bound
     
-    file_component: BasePermissionComponent = BasePermissionComponent(subject_file=remote_file, subject_file_owner=remote_directory, subject_user=remote_user, effect_duration=duration)
+    permission_component: BasePermissionComponent = BasePermissionComponent(subject_file=remote_file, subject_file_owner=remote_directory, subject_user=remote_user, effect_duration=duration)
 
-    await send_request(writer, header_component, session_manager.auth_component, file_component)
+    await send_request(writer, header_component, session_manager.auth_component, permission_component)
     response_header, _ = await process_response(reader, writer, CLIENT_CONFIG.read_timeout, REQUEST_CONSTANTS.header.max_bytesize)
 
     if response_header.code != SuccessFlags.SUCCESSFUL_GRANT.value:
@@ -36,9 +36,9 @@ async def grant_permission(reader: asyncio.StreamReader, writer: asyncio.StreamW
 
 async def revoke_permission(reader: asyncio.StreamReader, writer: asyncio.StreamWriter, remote_user: str, remote_directory: str, remote_file: str) -> None:
     header_component: BaseHeaderComponent = BaseHeaderComponent(version=CLIENT_CONFIG.version, category=CategoryFlag.PERMISSION, subcategory=PermissionFlags.REVOKE)
-    file_component: BasePermissionComponent = BasePermissionComponent(subject_file=remote_file, subject_file_owner=remote_directory, subject_user=remote_user)
+    permission_component: BasePermissionComponent = BasePermissionComponent(subject_file=remote_file, subject_file_owner=remote_directory, subject_user=remote_user)
 
-    await send_request(writer, header_component, session_manager.auth_component, file_component)
+    await send_request(writer, header_component, session_manager.auth_component, permission_component)
     response_header, response_body = await process_response(reader, writer, CLIENT_CONFIG.read_timeout, REQUEST_CONSTANTS.header.max_bytesize)
 
     if response_header.code != SuccessFlags.SUCCESSFUL_REVOKE.value:
@@ -60,9 +60,9 @@ async def transfer_ownership(reader: asyncio.StreamReader, writer: asyncio.Strea
 
 async def publicise_remote_file(reader: asyncio.StreamReader, writer: asyncio.StreamWriter, remote_directory: str, remote_file: str) -> None:
     header_component: BaseHeaderComponent = BaseHeaderComponent(version=CLIENT_CONFIG.version, category=CategoryFlag.PERMISSION, subcategory=PermissionFlags.PUBLICISE)
-    file_component: BasePermissionComponent = BasePermissionComponent(subject_file=remote_file, subject_file_owner=remote_directory)
+    permission_component: BasePermissionComponent = BasePermissionComponent(subject_file=remote_file, subject_file_owner=remote_directory)
 
-    await send_request(writer, header_component, session_manager.auth_component, file_component)
+    await send_request(writer, header_component, session_manager.auth_component, permission_component)
     response_header, _ = await process_response(reader, writer, CLIENT_CONFIG.read_timeout)
     if response_header.code != SuccessFlags.SUCCESSFUL_FILE_PUBLICISE.value:
         await display(f'Failed to publicise file {remote_directory}/{remote_file}.\n Code: {response_header.code}')
@@ -73,9 +73,9 @@ async def publicise_remote_file(reader: asyncio.StreamReader, writer: asyncio.St
 
 async def hide_remote_file(reader: asyncio.StreamReader, writer: asyncio.StreamWriter, remote_directory: str, remote_file: str):
     header_component: BaseHeaderComponent = BaseHeaderComponent(version=CLIENT_CONFIG.version, category=CategoryFlag.PERMISSION, subcategory=PermissionFlags.PUBLICISE)
-    file_component: BasePermissionComponent = BasePermissionComponent(subject_file=remote_file, subject_file_owner=remote_directory)
+    permission_component: BasePermissionComponent = BasePermissionComponent(subject_file=remote_file, subject_file_owner=remote_directory)
 
-    await send_request(writer, header_component, session_manager.auth_component, file_component)
+    await send_request(writer, header_component, session_manager.auth_component, permission_component)
     response_header, response_body = await process_response(reader, writer, CLIENT_CONFIG.read_timeout)
     if response_header.code != SuccessFlags.SUCCESSFUL_FILE_PUBLICISE.value:
         await display(f'Failed to hide file {remote_directory}/{remote_file}.\n Code: {response_header.code}')
