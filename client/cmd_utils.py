934229f110e9f4cd821bcea0145d41fadf1c565c
Parth Acharya <1008parth@gmail.com>
2025-07-15 22:10:22 +0530

    Fixes incorrect string casting logic in display

--- client/cmd_utils.py
+++ client/cmd_utils.py
@@ -2,10 +2,10 @@ import sys
 import aiofiles
 import itertools
 import asyncio
-from typing import Sequence
+from typing import Sequence, Union
 
-async def display(*args, sep=b' ', end=b'\n'):
-    write_buffer: bytes = sep.join(bytes(arg) for arg in args)
+async def display(*args: Union[str, bytes], sep=b' ', end=b'\n'):
+    write_buffer: bytes = sep.join(arg.encode('utf-8') if isinstance(arg, str) else arg for arg in args)
     async with aiofiles.open(sys.stdout.fileno(), mode='wb') as stdout:
         await stdout.write(write_buffer)
         await stdout.write(end)
