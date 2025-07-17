4558c4a8a9901642bc6ebe0fe0ed3ecd67855486
Parth Acharya <1008parth@gmail.com>
2025-07-17 22:23:41 +0530

    Updates SessionManager to stop relying on a separate jsonschema

--- client/session_manager.py
+++ client/session_manager.py
@@ -2,11 +2,8 @@ from models.request_model import BaseAuthComponent
 from models.response_models import ResponseBody
 from models.session_metadata import SessionMetadata
 
-import jsonschema
-
 class SessionManager:
-    def __init__(self, auth_response_schema: str):
-        self.auth_response_schema: str = auth_response_schema
+    def __init__(self):
         self.identity: str = None
         self.session_metadata: SessionMetadata = None
         self.auth_component: BaseAuthComponent = None
@@ -15,8 +12,9 @@ class SessionManager:
     def make_authorization_component(identity: str, password: str) -> BaseAuthComponent:
         return BaseAuthComponent(identity=identity, password=password)
 
-    def prepare_authentication_component(self, body: ResponseBody) -> None:
-        jsonschema.validate(body.contents, self.auth_response_schema, jsonschema.Draft202012Validator)
+    def create_authentication_component(self, token: bytes, digest: bytes, lifespan: float, last_refresh: float, valid_until: float, iteration: int) -> None:
+        self.session_metadata = SessionMetadata.from_response(token, digest, lifespan, last_refresh, valid_until, iteration)
 
-        self.session_metadata.update_digest(new_digest=body.contents['refresh_digest'])
+    def update_authentication_component(self, digest: bytes) -> None:
+        self.session_metadata.update_digest(new_digest=bytes)
         self.auth_component.refresh_digest = self.session_metadata.refresh_digest
