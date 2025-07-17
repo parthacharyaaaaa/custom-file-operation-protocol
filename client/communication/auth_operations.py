1b6fce9fae1cdc68018b6cbcc5d99daf50fc437c
Parth Acharya <1008parth@gmail.com>
2025-07-17 20:28:36 +0530

    Adds client method: create_remote_user

---
+++ client/communication/auth_operations.py
@@ -0,0 +1,32 @@
+import asyncio
+from traceback import format_exception_only
+
+from client.communication.outgoing import send_request
+from client.communication.incoming import process_response
+from client.config.constants import CLIENT_CONFIG
+from client.cmd.cmd_utils import display
+from client.cmd.message_strings import auth_messages
+
+from pydantic import ValidationError
+
+from models.request_model import BaseHeaderComponent, BaseAuthComponent
+from models.flags import CategoryFlag, AuthFlags
+from models.response_codes import SuccessFlags
+
+async def create_remote_user(reader: asyncio.StreamReader, writer: asyncio.StreamWriter, username: str, password: str) -> None:
+    try:
+        auth_component: BaseAuthComponent = BaseAuthComponent(identity=username, password=password)
+    except ValidationError as v:
+        await display(auth_messages.invalid_user_data(), format_exception_only(v), sep=b' : ')
+        return
+    
+    await send_request(writer=writer,
+                       header_component=BaseHeaderComponent(version=CLIENT_CONFIG.version, category=CategoryFlag.AUTH, subcategory=AuthFlags.REGISTER),
+                       auth_component=auth_component)
+    response_header, _ = await process_response(reader, writer, CLIENT_CONFIG.read_timeout)
+    if response_header.code != SuccessFlags.SUCCESSFUL_USER_CREATION.value:
+        await display(auth_messages.failed_auth_operation(operation=AuthFlags.REGISTER, code=response_header.code))
+        return
+    
+    await display()
+
