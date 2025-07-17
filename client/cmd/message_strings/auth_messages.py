d5f793e15bec5fb25a6f596bae1dfd0799c48988
Parth Acharya <1008parth@gmail.com>
2025-07-17 20:34:43 +0530

    Updates invalid_user_data() str factory to accept pydantic.ValidationError as an optional param

--- client/cmd/message_strings/auth_messages.py
+++ client/cmd/message_strings/auth_messages.py
@@ -1,7 +1,10 @@
+from typing import Optional
 from models.flags import AuthFlags
+from pydantic import ValidationError
+from traceback import format_exception_only
 
-def invalid_user_data() -> str:
-    return 'Invalid user data'
+def invalid_user_data(exception: Optional[ValidationError] = None) -> str:
+    return ':'.join(['Invalid user data', format_exception_only[exception][0] if exception else ''])
 
 def failed_auth_operation(operation: AuthFlags, code: str) -> str:
     return '\n'.join([f'Code {code}: Failed auth operation',
