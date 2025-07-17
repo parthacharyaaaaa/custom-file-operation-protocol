528b08876051c87e423deb2aa19d438714a48d55
Parth Acharya <1008parth@gmail.com>
2025-07-17 20:53:39 +0530

    Adds client method: delete_remote_user

--- client/communication/auth_operations.py
+++ client/communication/auth_operations.py
@@ -1,11 +1,11 @@
 import asyncio
-from traceback import format_exception_only
 
 from client.communication.outgoing import send_request
 from client.communication.incoming import process_response
 from client.config.constants import CLIENT_CONFIG
 from client.cmd.cmd_utils import display
 from client.cmd.message_strings import auth_messages
+from client.cmd.message_strings import general_messages
 
 from pydantic import ValidationError
 
@@ -17,7 +17,7 @@ async def create_remote_user(reader: asyncio.StreamReader, writer: asyncio.Strea
     try:
         auth_component: BaseAuthComponent = BaseAuthComponent(identity=username, password=password)
     except ValidationError as v:
-        await display(auth_messages.invalid_user_data(), format_exception_only(v), sep=b' : ')
+        await display(auth_messages.invalid_user_data(v), sep=b' : ')
         return
     
     await send_request(writer=writer,
@@ -30,3 +30,29 @@ async def create_remote_user(reader: asyncio.StreamReader, writer: asyncio.Strea
     
     await display()
 
+async def delete_remote_user(reader: asyncio.StreamReader, writer: asyncio.StreamWriter, username: str, password: str) -> None:
+    try:
+        auth_component: BaseAuthComponent = BaseAuthComponent(identity=username, password=password)
+    except ValidationError as v:
+        await display(auth_messages.invalid_user_data(v))
+        return
+    
+    await send_request(writer,
+                       header_component=BaseHeaderComponent(version=CLIENT_CONFIG.version, category=CategoryFlag.AUTH, subcategory=AuthFlags.DELETE),
+                       auth_component=auth_component)
+    
+    response_header, response_body = await process_response(reader, writer, CLIENT_CONFIG.read_timeout)
+    if response_header.code != SuccessFlags.SUCCESSFUL_USER_DELETION:
+        await display(auth_messages.failed_auth_operation(AuthFlags.DELETE, response_header.code))
+        return
+
+    deleted_count: int = response_body.contents.get('deleted_count')
+    if not deleted_count:
+        await display(general_messages.missing_response_claim('deleted_count'))
+    deleted_files: list[str] = response_body.contents.get('deleted_files')
+    if not deleted_files:
+        await display(general_messages.missing_response_claim('deleted_files'))
+    if actual_fcount:=len(deleted_files) != deleted_count:
+        await display(general_messages.malformed_response_body(message=auth_messages.filecount_mismatch(deleted_count, actual_fcount)))
+
+    await display(auth_messages.successful_user_deletion(username, deleted_count, deleted_files))
