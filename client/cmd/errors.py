15578562e29e568df2b45c0e49bcd9b4ec369051
Parth Acharya <1008parth@gmail.com>
2025-07-23 08:10:55 +0530

    Adds base boilerplate for cmd-related exceptions

--- client/cmd/errors.py
+++ client/cmd/errors.py
@@ -1,5 +1,11 @@
+from typing import Optional
+
 class CommandException(Exception):
     description: str = 'Invalid command issued'
 
-    def __init__(self, *args):
-        super().__init__(*args)
+    def __init__(self, description: Optional[str] = None):
+        super().__init__(description or self.__class__.description)
+
+
+class InvalidAuthenticationState(CommandException):
+    description: str = 'Invalid authentication state'
