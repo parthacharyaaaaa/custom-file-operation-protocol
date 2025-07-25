10747fd83b9f5110dfdff97e931d927e29c2ab4e
Parth Acharya <1008parth@gmail.com>
2025-07-25 22:52:42 +0530

    Updates send_request to accept streaming_lock and a contention timeout as params, and use asyncio.wait_for when acquiring the lock

--- client/communication/outgoing.py
+++ client/communication/outgoing.py
@@ -1,18 +1,25 @@
 import asyncio
-from typing import Optional, Union
+from typing import Optional, Union, Literal
 from models.request_model import BaseHeaderComponent, BaseAuthComponent, BaseFileComponent, BasePermissionComponent
-from client.bootup import stream_lock
 
-async def send_request(writer: asyncio.StreamWriter, header_component: BaseHeaderComponent, auth_component: Optional[BaseAuthComponent] = None, body_component: Optional[Union[BaseFileComponent, BasePermissionComponent]] = None) -> None:
+async def send_request(writer: asyncio.StreamWriter,
+                       header_component: BaseHeaderComponent,
+                       streaming_lock: asyncio.Lock,
+                       lock_contention_timmeout: float = 3.0,
+                       auth_component: Optional[BaseAuthComponent] = None,
+                       body_component: Optional[Union[BaseFileComponent, BasePermissionComponent]] = None) -> None:
     auth_stream = b'' if not auth_component else auth_component.model_dump_json().encode('utf-8')
     body_stream = b'' if not body_component else body_component.model_dump_json().encode('utf-8')
 
     header_component.auth_size = len(auth_stream)
     header_component.body_size = len(body_stream)
 
-    async with stream_lock:
+    acquired: Literal[True] = asyncio.wait_for(streaming_lock.acquire(), lock_contention_timmeout)
+    try:
         writer.write(header_component.model_dump_json().encode('utf-8'))
         writer.write(auth_stream)
         writer.write(body_stream)
 
         await writer.drain()
+    finally:
+        streaming_lock.release()
