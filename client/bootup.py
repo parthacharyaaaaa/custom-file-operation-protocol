67c72c3fe46d90184625fbe806577fc63c6fd59c
Parth Acharya <1008parth@gmail.com>
2025-07-25 20:45:25 +0530

    Moves config object initializer function to client bootup module

--- client/bootup.py
+++ client/bootup.py
@@ -1,6 +1,17 @@
-import asyncio
-from session_manager import SessionManager
+import os
+from typing import Any
+import pytomlpp
 
+from client.config.constants import ClientConfig
+from client.session_manager import SessionManager
 
-session_manager: SessionManager = None
-stream_lock: asyncio.Lock = asyncio.Lock()
+__all__ = ('init_session_manager', 'init_client_configurations',)
+
+def init_session_manager() -> SessionManager:
+    return SessionManager()
+
+def init_client_configurations() -> ClientConfig:
+    constants_mapping: dict[str, Any] = pytomlpp.load(os.path.join(os.path.dirname(__file__), 'config', 'constants.toml'))
+    client_config = ClientConfig.model_validate(constants_mapping)
+
+    return client_config
