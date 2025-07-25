be560fa4e2c6a44b63f276ce553fcf34a71bbd4a
Parth Acharya <1008parth@gmail.com>
2025-07-25 22:17:28 +0530

    Adds ssl_handshake_timeout field to ClientConfig

--- client/config/constants.py
+++ client/config/constants.py
@@ -7,3 +7,4 @@ __all__ = ('ClientConfig',)
 class ClientConfig(BaseModel):
     version: Annotated[str, Field(frozen=True, pattern=REQUEST_CONSTANTS.header.version_regex)]
     read_timeout: Annotated[float, Field(frozen=True, ge=0)]
+    ssl_handshake_timeout: Annotated[float, Field(frozen=True, ge=0)]
