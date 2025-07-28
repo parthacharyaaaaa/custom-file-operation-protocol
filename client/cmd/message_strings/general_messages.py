2fd6e73473cbe584e31b6ad7c4d31633f163bc55
Parth Acharya <1008parth@gmail.com>
2025-07-28 21:14:39 +0530

    Fixes inverted logic for malformed_response_body str factory

--- client/cmd/message_strings/general_messages.py
+++ client/cmd/message_strings/general_messages.py
@@ -4,4 +4,4 @@ def missing_response_claim(*expected_claims: str) -> str:
     return f'Malformed response body, expected claims: {expected_claims}'
 
 def malformed_response_body(message: Optional[str] = None) -> str:
-    return "\n".join((f'Malformed response body:', "Unknown cause. Possible data type mismatch or illogical values" if message else message,))
+    return "\n".join((f'Malformed response body:', "Unknown cause. Possible data type mismatch or illogical values" if not message else message))
