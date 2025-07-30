dfcd6a139435e2004af760f00d3679b83fc21528
Parth Acharya <1008parth@gmail.com>
2025-07-30 10:32:02 +0530

    Updates str factory

--- client/cmd/message_strings/file_messages.py
+++ client/cmd/message_strings/file_messages.py
@@ -13,7 +13,7 @@ def succesful_file_creation(remote_directory: str, remote_file: str, iso_epoch: 
     return f'Code {code or SuccessFlags.SUCCESSFUL_FILE_CREATION.value}: Created file {remote_directory}/{remote_file} at {iso_epoch}'
 
 def succesful_file_deletion(remote_directory: str, remote_file: str, revoked_info: Sequence[dict[str, Any]], deletion_time_string: Optional[str] = None, code: Optional[SuccessFlags] = None) -> str:
-    revocation_info_string: str = '\n---\n'.join(format_dict(i) for i in revoked_info)
+    revocation_info_string: str = '\n---\n'.join(format_dict(i) for i in revoked_info) if revoked_info else "No revocation info available"
     return (f'Code {code or SuccessFlags.SUCCESSFUL_FILE_DELETION}: Deleted file {remote_directory}/{remote_file} at {deletion_time_string or "N/A"}\n{revocation_info_string}')
 
 def successful_file_amendment(remote_directory: str, remote_file: str, code: Optional[SuccessFlags] = None) -> str:
