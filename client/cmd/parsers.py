d559fc87d949b70433003a92536c41c5a4573219
Parth Acharya <1008parth@gmail.com>
2025-08-01 00:02:04 +0530

    Adds argument parsers for different operations

--- client/cmd/parsers.py
+++ client/cmd/parsers.py
@@ -1,16 +1,47 @@
-from traceback import format_exception_only
+import argparse
 from typing import Iterable, Sequence
 
+from client import cli_parser
 from client.cmd import cmd_utils
 import client.cmd.errors as cmd_exc
 from client.cmd.commands import AuthCommands, FileCommands, PermissionCommands
-from client.cmd.commands import GeneralModifierCommands
+from client.cmd.commands import GeneralModifierCommands, FileModifierCommands, AuthModifierCommands, PermissionCommands
 
 from models.request_model import BaseAuthComponent, BaseFileComponent, BasePermissionComponent
 from models.constants import REQUEST_CONSTANTS
 from models.flags import PermissionFlags
 
 import pydantic
+
+generic_modifier_parser: argparse.ArgumentParser = argparse.ArgumentParser(prog='modifier_commands')
+for modifier in GeneralModifierCommands:
+    generic_modifier_parser.add_argument(f'-{modifier.value.lower()}', help=None, action='store_true')
+
+#NOTE: For the generic filedir_parser, the action for 'directory' will have a default value injected at runtime based on the remote session
+filedir_parser: argparse.ArgumentParser = argparse.ArgumentParser(prog='filedir_parser', parents=[generic_modifier_parser], add_help=False)
+filedir_parser.add_argument('file', type=cli_parser.parse_filename)
+filedir_parser.add_argument('directory', type=cli_parser.parse_dir)
+
+file_operation_parser: argparse.ArgumentParser = argparse.ArgumentParser(prog='file_io_parser', parents=[filedir_parser], add_help=False)
+file_operation_parser.add_argument('--chunk', required=False, type=cli_parser.parse_non_negative_int)
+file_operation_parser.add_argument('--until', required=False, type=cli_parser.parse_non_negative_int)
+file_operation_parser.add_argument('--pos', required=False, type=cli_parser.parse_non_negative_int)
+
+for file_modifier in FileModifierCommands:
+    file_operation_parser.add_argument(f'-{file_modifier.value.lower()}', help=None, action='store_true')
+
+auth_operation_parser: argparse.ArgumentParser = argparse.ArgumentParser(prog='_auth_commands', parents=[generic_modifier_parser], add_help=False)
+auth_operation_parser.add_argument('username', type=cli_parser.parse_username_arg)
+auth_operation_parser.add_argument('password', type=cli_parser.parse_password_arg)
+
+for auth_modifier in AuthModifierCommands:
+    auth_operation_parser.add_argument(f'-{auth_modifier.value.lower()}', help=None, action='store_true')
+
+permission_parser: argparse.ArgumentParser = argparse.ArgumentParser(prog='permission_parser', parents=[filedir_parser], add_help=False)
+permission_parser.add_argument('user', type=cli_parser.parse_username_arg, default=None)
+
+for permisison_modifier in PermissionCommands:
+    auth_operation_parser.add_argument(f'-{permisison_modifier.value.lower()}', help=None, action='store_true')
 
 async def parse_modifiers(tokens: Iterable[str], *expected_modifiers: GeneralModifierCommands, raise_on_unexpected: bool = True) -> list[bool]:
     '''Parse a given command for any additional modifiers provided at the end
