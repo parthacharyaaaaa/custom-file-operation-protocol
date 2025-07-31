ee5b0aac145199ae0f7e876b460a627787713f0a
Parth Acharya <1008parth@gmail.com>
2025-08-01 01:18:12 +0530

    Creates dedicated module for parsing helper functions

--- client/cmd/parsers.py
+++ client/cmd/parsers.py
@@ -1,34 +1,34 @@
-import argparse
+from client.parsing.explicit_argument_parser import ExplicitArgumentParser
 
-from client import cli_parser
+from client.parsing import arg_parsers
 from client.cmd.commands import GeneralModifierCommands, FileModifierCommands, AuthModifierCommands, PermissionModifierCommands
 
-generic_modifier_parser: argparse.ArgumentParser = argparse.ArgumentParser(prog='modifier_commands')
+generic_modifier_parser: ExplicitArgumentParser = ExplicitArgumentParser(prog='modifier_commands')
 for modifier in GeneralModifierCommands:
     generic_modifier_parser.add_argument(f'-{modifier.value.lower()}', help=None, action='store_true')
 
 #NOTE: For the generic filedir_parser, the action for 'directory' will have a default value injected at runtime based on the remote session
-filedir_parser: argparse.ArgumentParser = argparse.ArgumentParser(prog='filedir_parser', parents=[generic_modifier_parser], add_help=False)
-filedir_parser.add_argument('file', type=cli_parser.parse_filename)
-filedir_parser.add_argument('directory', type=cli_parser.parse_dir)
+filedir_parser: ExplicitArgumentParser = ExplicitArgumentParser(prog='filedir_parser', parents=[generic_modifier_parser], add_help=False)
+filedir_parser.add_argument('file', type=arg_parsers.parse_filename)
+filedir_parser.add_argument('directory', type=arg_parsers.parse_dir)
 
-file_operation_parser: argparse.ArgumentParser = argparse.ArgumentParser(prog='file_io_parser', parents=[filedir_parser], add_help=False)
-file_operation_parser.add_argument('--chunk', required=False, type=cli_parser.parse_non_negative_int)
-file_operation_parser.add_argument('--until', required=False, type=cli_parser.parse_non_negative_int)
-file_operation_parser.add_argument('--pos', required=False, type=cli_parser.parse_non_negative_int)
+file_operation_parser: ExplicitArgumentParser = ExplicitArgumentParser(prog='file_io_parser', parents=[filedir_parser], add_help=False)
+file_operation_parser.add_argument('--chunk', required=False, type=arg_parsers.parse_non_negative_int)
+file_operation_parser.add_argument('--until', required=False, type=arg_parsers.parse_non_negative_int)
+file_operation_parser.add_argument('--pos', required=False, type=arg_parsers.parse_non_negative_int)
 
 for file_modifier in FileModifierCommands:
     file_operation_parser.add_argument(f'-{file_modifier.value.lower()}', help=None, action='store_true')
 
-auth_operation_parser: argparse.ArgumentParser = argparse.ArgumentParser(prog='_auth_commands', parents=[generic_modifier_parser], add_help=False)
-auth_operation_parser.add_argument('username', type=cli_parser.parse_username_arg)
-auth_operation_parser.add_argument('password', type=cli_parser.parse_password_arg)
+auth_operation_parser: ExplicitArgumentParser = ExplicitArgumentParser(prog='_auth_commands', parents=[generic_modifier_parser], add_help=False)
+auth_operation_parser.add_argument('username', type=arg_parsers.parse_username_arg)
+auth_operation_parser.add_argument('password', type=arg_parsers.parse_password_arg)
 
 for auth_modifier in AuthModifierCommands:
     auth_operation_parser.add_argument(f'-{auth_modifier.value.lower()}', help=None, action='store_true')
 
-permission_parser: argparse.ArgumentParser = argparse.ArgumentParser(prog='permission_parser', parents=[filedir_parser], add_help=False)
-permission_parser.add_argument('user', type=cli_parser.parse_username_arg, default=None)
+permission_parser: ExplicitArgumentParser = ExplicitArgumentParser(prog='permission_parser', parents=[filedir_parser], add_help=False)
+permission_parser.add_argument('user', type=arg_parsers.parse_username_arg, default=None)
 
 for permisison_modifier in PermissionModifierCommands:
     permission_parser.add_argument(f'-{permisison_modifier.value.lower()}', help=None, action='store_true')
---
+++ client/parsing/arg_parsers.py
@@ -0,0 +1,53 @@
+'''Parsers for individual arguments'''
+
+import re
+from models.constants import REQUEST_CONSTANTS
+
+def parse_filename(filename: str) -> str:
+    if not re.match(r'(.\w*)+', (filename:=filename.strip())):
+        raise ValueError('Invalid filename')
+    return filename
+
+def parse_dir(dir: str) -> str:
+    if not (dir:=dir.strip()).isalnum():
+        raise ValueError('Invalid directory name')
+    return dir
+
+def parse_non_negative_int(arg: str) -> int:
+    if not (arg:=arg.strip()).isnumeric():
+        raise ValueError(f'Non-numeric argument ({arg}) given')
+    num: int = int(arg)
+    if num < 0:
+        raise ValueError(f'Non-negative integer expected, got ({num})')
+    return num
+
+def parse_host_arg(host: str) -> str:
+    if not re.match(r'^((25[0-5]|(2[0-4]|1\d|[1-9]|)\d)\.?\b){4}$', host):
+        raise ValueError(f'Invalid IP (v4/v6) address {host} provided')
+    return host
+
+def parse_port_arg(arg: str) -> int:
+    if not arg.isnumeric():
+        raise TypeError('Port must be numeric')
+    
+    port: int = int(arg)
+    if not (0 <= port <= 65_535):
+        raise ValueError('TCP port must be between range 0 and 65,535')
+    
+    return port
+
+def parse_password_arg(arg: str) -> str:
+    if not (REQUEST_CONSTANTS.auth.password_range[0] <= len(arg) <= REQUEST_CONSTANTS.auth.password_range[1]):
+        raise ValueError(f'Invalid range for password ({len(arg)}), must be in range {REQUEST_CONSTANTS.auth.password_range}')
+    
+    return arg
+
+def parse_username_arg(arg: str) -> str:
+    arg = arg.strip()
+    if not (REQUEST_CONSTANTS.auth.username_range[0] <= len(arg) <= REQUEST_CONSTANTS.auth.username_range[1]):
+        raise ValueError(f'Invalid range for password ({len(arg)}), must be in range {REQUEST_CONSTANTS.auth.username_range}')
+    
+    if not re.match(REQUEST_CONSTANTS.auth.username_regex, arg):
+        raise ValueError(f'Invalid username format: {arg}')
+        
+    return arg
