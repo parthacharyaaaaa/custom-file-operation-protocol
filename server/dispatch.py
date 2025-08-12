f50c753940911c9e8d9b601ac50cc702ef5963ce
Parth Acharya <1008parth@gmail.com>
2025-08-12 23:49:57 +0530

    Adds INFO category to TOP_LEVEL_REQUEST_MAPPING

--- server/dispatch.py
+++ server/dispatch.py
@@ -7,7 +7,7 @@ from models.response_models import ResponseHeader, ResponseBody
 from models.request_model import BaseHeaderComponent
 
 from server.authz.auth_handler import top_auth_handler
-from server.comms_utils.outgoing import send_heartbeat
+from server.info_ops.info_handler import top_info_handler
 from server.dependencies import ServerSingletonsRegistry
 from server.file_ops.file_handler import top_file_handler
 from server.permission_ops.permission_handler import top_permission_handler
@@ -15,7 +15,7 @@ from server.permission_ops.permission_handler import top_permission_handler
 __all__ = ('TOP_LEVEL_REQUEST_MAPPING',)
 
 TOP_LEVEL_REQUEST_MAPPING: MappingProxyType[int, Callable[[asyncio.StreamReader, BaseHeaderComponent, ServerSingletonsRegistry], Coroutine[Any, Any, tuple[ResponseHeader, Optional[ResponseBody]]]]] = (
-    MappingProxyType({CategoryFlag.AUTH : top_auth_handler,
-                      CategoryFlag.HEARTBEAT : send_heartbeat,
-                      CategoryFlag.FILE_OP : top_file_handler,
-                      CategoryFlag.PERMISSION : top_permission_handler}))
+    MappingProxyType({CategoryFlag.AUTH         : top_auth_handler,
+                      CategoryFlag.INFO         : top_info_handler,
+                      CategoryFlag.FILE_OP      : top_file_handler,
+                      CategoryFlag.PERMISSION   : top_permission_handler}))
