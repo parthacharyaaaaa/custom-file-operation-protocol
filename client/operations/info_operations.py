81d06a021f674408fb2943687a328a934fd99079
Parth Acharya <1008parth@gmail.com>
2025-08-13 22:29:03 +0530

    Adds send_info_query generic method for INFO operations

--- client/operations/info_operations.py
+++ client/operations/info_operations.py
@@ -6,9 +6,10 @@ from client.config import constants as client_constants
 from client.communication import incoming, outgoing
 from client.cmd import cmd_utils
 
-from models.request_model import BaseHeaderComponent
-from models.flags import CategoryFlag
+from models.request_model import BaseHeaderComponent, BaseInfoComponent
+from models.flags import CategoryFlag, InfoFlags
 from models.response_codes import SuccessFlags
+from models.constants import UNAUTHENTICATED_INFO_OPERATIONS
 
 async def send_heartbeat(reader: asyncio.StreamReader, writer: asyncio.StreamWriter,
                          client_config: client_constants.ClientConfig, session_master: session_manager.SessionManager,
@@ -25,3 +26,25 @@ async def send_heartbeat(reader: asyncio.StreamReader, writer: asyncio.StreamWri
         # TODO: Add generic message factories
     
     await cmd_utils.display('Doki Doki')
+
+async def send_info_query(reader: asyncio.StreamReader, writer: asyncio.StreamWriter,
+                          client_config: client_constants.ClientConfig, session_master: session_manager.SessionManager,
+                          subcategory_flags: InfoFlags, resource: str,
+                          end_connection: bool = False) -> None:
+    header_component: BaseHeaderComponent = operational_utils.make_header_component(client_config, session_master, CategoryFlag.INFO, subcategory_flags, finish=end_connection)
+    info_component: BaseInfoComponent = BaseInfoComponent(subject_resource=resource)
+    extracted_subcategory: InfoFlags = InfoFlags(subcategory_flags & InfoFlags.OPERATION_EXTRACTION_BITS)
+
+    await outgoing.send_request(writer=writer,
+                                header_component=header_component,
+                                auth_component=None if extracted_subcategory in UNAUTHENTICATED_INFO_OPERATIONS else session_master.auth_component,
+                                body_component=info_component)
+
+    response_header, response_body = await incoming.process_response(reader, writer, client_config.read_timeout)
+    if response_header.code != SuccessFlags.SUCCESSFUL_QUERY_ANSWER.value:
+        await cmd_utils.display(f'{response_header.code}: Failed to perform query operation: {extracted_subcategory._name_}')
+        return
+    
+    await cmd_utils.display(cmd_utils.format_dict(response_body.contents)
+                            if response_body.contents
+                            else f'No available information for operation {extracted_subcategory._name_} on resource {resource}')
