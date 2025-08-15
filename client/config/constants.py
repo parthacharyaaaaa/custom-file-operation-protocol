642478283c00c62970f2f0cb617a8fce544dd7c1
Parth Acharya <1008parth@gmail.com>
2025-08-15 13:36:21 +0530

    Updates ClientConfig to store server_fingerprints_filepath and ciphers

--- client/config/constants.py
+++ client/config/constants.py
@@ -1,6 +1,8 @@
-from typing import Annotated
-from pydantic import BaseModel, Field
+from typing import Annotated, Union
+from pydantic import BaseModel, Field, field_validator
 from models.constants import REQUEST_CONSTANTS
+
+from pathlib import Path
 
 __all__ = ('ClientConfig',)
 
@@ -9,3 +11,15 @@ class ClientConfig(BaseModel):
     read_timeout: Annotated[float, Field(frozen=True, ge=0)]
     ssl_handshake_timeout: Annotated[float, Field(frozen=True, ge=0)]
     heartbeat_interval: Annotated[float, Field(ge=0)]
+    server_fingerprints_filepath: Path
+    ciphers: Annotated[str, Field(frozen=True)]
+
+    @field_validator('server_fingerprints_filepath', mode='before')
+    @classmethod
+    def process_fingerprints_filename(cls, path: Union[str, Path]) -> Path:
+        return Path(path) if isinstance(path, str) else path
+    
+    @field_validator('ciphers', mode='before')
+    @classmethod
+    def prevaldiate_ciphers(cls, ciphers: str) -> str:
+        return ciphers.strip().upper()
