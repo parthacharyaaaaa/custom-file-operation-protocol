621acadfc64e633a0dba5d42a47c1a84d3108368
Parth Acharya <1008parth@gmail.com>
2025-08-22 00:15:03 +0530

    Fixes bugs in server certificate rotation logic

--- server/tls/credentials.py
+++ server/tls/credentials.py
@@ -18,10 +18,9 @@ from server.config.server_config import ServerConfig
 
 __all__ = ('generate_self_signed_credentials', 'generate_rollover_token', 'make_server_ssl_context', 'load_credentials', 'rotate_server_certificates')
 
-def generate_self_signed_credentials(credentials_directory: Path,
-                                     dns_name: str = 'localhost',
-                                     cert_filename: Optional[str] = 'certfile.crt',
-                                     key_filename: Optional[str] = 'keyfile.pem') -> tuple[x509.Certificate, ec.EllipticCurvePrivateKey]:
+def generate_self_signed_credentials(cert_filepath: Path,
+                                     key_filepath: Path,
+                                     dns_name: str = 'localhost') -> tuple[x509.Certificate, ec.EllipticCurvePrivateKey]:
     private_key: ec.EllipticCurvePrivateKey = ec.generate_private_key(ec.SECP256R1())
     
     subject = issuer = x509.Name([
@@ -43,20 +42,19 @@ def generate_self_signed_credentials(credentials_directory: Path,
         .sign(private_key, hashes.SHA256())
     )
 
-    Path.mkdir(credentials_directory, exist_ok=True)
-    certpath: Path = Path.joinpath(credentials_directory, cert_filename)
-    keypath: Path = Path.joinpath(credentials_directory, key_filename)
-
+    certfile_exists, keyfile_exists = cert_filepath.exists(), key_filepath.exists()
     try:
-        with open(certpath, 'wb') as certfile:
+        with open(cert_filepath, 'wb') as certfile:
             certfile.write(cert.public_bytes(encoding=serialization.Encoding.PEM))
-        with open(keypath, 'wb') as keyfile:
+        with open(key_filepath, 'wb') as keyfile:
             keyfile.write(private_key.private_bytes(encoding=serialization.Encoding.PEM,
                                                     format=serialization.PrivateFormat.TraditionalOpenSSL,
                                                     encryption_algorithm=serialization.NoEncryption()))
     except Exception as e:
-        Path.unlink(certpath, missing_ok=True)
-        Path.unlink(keypath, missing_ok=True)
+        if not certfile_exists:
+            Path.unlink(cert_filepath, missing_ok=True)
+        if not keyfile_exists:
+            Path.unlink(keyfile_exists, missing_ok=True)
         raise e
     
     return cert, private_key
@@ -87,18 +85,19 @@ def generate_rollover_token(new_cert: x509.Certificate,
     old_pubkey_hash: str = hashlib.sha256(old_cert.public_key().public_bytes(encoding=serialization.Encoding.DER, format=serialization.PublicFormat.SubjectPublicKeyInfo)).hexdigest()
     new_pubkey_hash: str = hashlib.sha256(new_cert.public_key().public_bytes(encoding=serialization.Encoding.DER, format=serialization.PublicFormat.SubjectPublicKeyInfo)).hexdigest()
     nonce: str = secrets.token_hex(nonce_length)
-    json.dump(fp=output_path,
-              indent=4,
-              obj={
-                  'server' : host,
-                  'old_pubkey_hash' : old_pubkey_hash,
-                  'new_pubkey_hash' : new_pubkey_hash,
-                  'issued_at' : issuance,
-                  'not_before' : issuance+grace_period,
-                  'reason' : reason,
-                  'signature' : old_key.sign(old_pubkey_hash+new_pubkey_hash+nonce).hex(),
-                  'nonce' : nonce
-                  })
+    signature: Final[str] = old_key.sign(data=bytes.fromhex(old_pubkey_hash)+bytes.fromhex(new_pubkey_hash)+bytes.fromhex(nonce),
+                                         signature_algorithm=ec.ECDSA(hashes.SHA256())).hex()
+
+
+    rollover_data: Final[dict[str, str|float]] = {'server' : host,
+                                                  'old_pubkey_hash' : old_pubkey_hash,
+                                                  'new_pubkey_hash' : new_pubkey_hash,
+                                                  'issued_at' : issuance,
+                                                  'not_before' : issuance+grace_period,
+                                                  'reason' : reason,
+                                                  'signature' : signature,
+                                                  'nonce' : nonce}
+    output_path.write_text(json.dumps(rollover_data, indent=4), encoding='utf-8')
 
 def load_credentials(credentials_directory: Path,
                      cert_filename: Optional[str] = 'certfile.crt',
@@ -114,7 +113,7 @@ def load_credentials(credentials_directory: Path,
     certificate_bytes: bytes = Path.read_bytes(certificate_filepath)
     private_key_bytes: bytes = Path.read_bytes(key_filepath)
 
-    private_key: PrivateKeyTypes = serialization.load_pem_private_key(private_key_bytes)
+    private_key: PrivateKeyTypes = serialization.load_pem_private_key(private_key_bytes, password=None)
     if not isinstance(private_key, ec.EllipticCurvePrivateKey):
         raise ValueError(f"Expected private key to be EC key, got instance of {private_key.__class__}")
 
@@ -122,21 +121,20 @@ def load_credentials(credentials_directory: Path,
 
 def rotate_server_certificates(server_config: ServerConfig,
                                reason: str = 'periodic rotation') -> ssl.SSLContext:
-    credentials_directory: Final[Path] = server_config.files_directory / server_config.credentials_dirname
-    old_certificate, old_key = load_credentials(credentials_directory=credentials_directory,
-                                                cert_filename=server_config.certificate_filename,
-                                                key_filename=server_config.key_filename)
+
+    old_certificate, old_key = load_credentials(credentials_directory=server_config.certificate_filepath.parent,
+                                                cert_filename=server_config.certificate_filepath,
+                                                key_filename=server_config.key_filepath)
     
-    new_certificate, new_key = generate_self_signed_credentials(credentials_directory=credentials_directory,
-                                                                cert_filename=server_config.certificate_filename,
-                                                                key_filename=server_config.key_filename,
+    new_certificate, new_key = generate_self_signed_credentials(cert_filepath=server_config.certificate_filepath,
+                                                                key_filepath=server_config.key_filepath,
                                                                 dns_name=str(server_config.host))
     
     generate_rollover_token(new_cert=new_certificate, old_cert=old_certificate, old_key=old_key,
                             nonce_length=server_config.rollover_token_nonce_length, host=str(server_config.host), grace_period=server_config.rollover_grace_window,
-                            output_path=server_config.files_directory / server_config.rollover_token_filename,
+                            output_path=server_config.rollover_data_filepath,
                             reason=reason)
     
-    return make_server_ssl_context(certfile=credentials_directory,
-                                   keyfile=credentials_directory / server_config.key_filename,
+    return make_server_ssl_context(certfile=server_config.certificate_filepath,
+                                   keyfile=server_config.key_filepath,
                                    ciphers=server_config.ciphers)
