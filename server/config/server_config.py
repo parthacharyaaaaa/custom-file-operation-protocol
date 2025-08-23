084b07efe66646c7a09925b8b183e002962b5431
Parth Acharya <1008parth@gmail.com>
2025-08-23 19:18:03 +0530

    Updates server config to include field: rollover_history_length

--- server/config/server_config.py
+++ server/config/server_config.py
@@ -32,6 +32,7 @@ class ServerConfig(BaseModel):
     rollover_grace_window: Annotated[float, Field(frozen=True, ge=1)]
     rollover_check_poll_interval: Annotated[float, Field(ge=1)]
     rollover_token_nonce_length: Annotated[int, Field(ge=1, frozen=True)]
+    rollover_history_length: Annotated[int, Field(ge=0, frozen=True)]
 
     # Database
     max_connections: tuple[Annotated[int, Field(ge=1)],
