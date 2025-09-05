e69101c79e1c2390759a3d78a3b39d1142568816
Parth Acharya <1008parth@gmail.com>
2025-09-06 00:45:16 +0530

    Adds typing module to server package

---
+++ server/typing.py
@@ -0,0 +1,40 @@
+'''Typing support for server-specific data structures'''
+
+import asyncio
+from typing import Any, Callable, Coroutine, Optional, TypeAlias, Union
+
+from server.dependencies import (GlobalFileLockType, GlobalLogQueueType,
+                                 GlobalAmendCacheType, GlobalDeleteCacheType, GlobalReadCacheType,
+                                 ServerConfig, ConnectionPoolManager, UserManager,
+                                 ServerSingletonsRegistry)
+
+from models.response_models import ResponseHeader, ResponseBody
+from models.request_model import BaseHeaderComponent, BaseAuthComponent, BaseInfoComponent, BaseFileComponent, BasePermissionComponent
+
+__all__ = ('PermissionSubhandler',
+           'InfoSubhandler',
+           'AuthSubhandler',
+           'FileSubhandler',
+           'SubhandlerResponse',
+           'ServerSingleton',
+           'TopRequestHandler')
+
+ServerSingleton: TypeAlias = Union[GlobalReadCacheType, GlobalAmendCacheType, GlobalDeleteCacheType,
+                                   GlobalFileLockType, GlobalLogQueueType, ServerConfig,
+                                   ConnectionPoolManager, UserManager]
+
+PermissionSubhandler: TypeAlias = Callable[[BaseHeaderComponent, BaseAuthComponent, BasePermissionComponent],
+                                      Coroutine[Any, Any, tuple[ResponseHeader, Optional[ResponseBody]]]]
+
+InfoSubhandler: TypeAlias = Callable[[BaseHeaderComponent, BaseAuthComponent, BaseInfoComponent],
+                                      Coroutine[Any, Any, tuple[ResponseHeader, Optional[ResponseBody]]]]
+
+AuthSubhandler: TypeAlias = Callable[[BaseHeaderComponent, BaseAuthComponent],
+                                      Coroutine[Any, Any, tuple[ResponseHeader, Optional[ResponseBody]]]]
+
+FileSubhandler: TypeAlias = Callable[[BaseHeaderComponent, BaseAuthComponent, BaseFileComponent],
+                                      Coroutine[Any, Any, tuple[ResponseHeader, Optional[ResponseBody]]]]
+
+SubhandlerResponse: TypeAlias = Coroutine[Any, Any, tuple[ResponseHeader, Optional[ResponseBody]]]
+
+TopRequestHandler: TypeAlias = Callable[[asyncio.StreamReader, BaseHeaderComponent, ServerSingletonsRegistry], SubhandlerResponse]
