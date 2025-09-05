2ad76c1ed0fddd96ab0f2855c7e95febb883c4ed
Parth Acharya <1008parth@gmail.com>
2025-09-06 00:49:19 +0530

    Moves request subhandler mappings to dispatch module
    Also updates type annotations to use server.typing type aliases

--- server/dispatch.py
+++ server/dispatch.py
@@ -1,21 +1,67 @@
-import asyncio
-from typing import Any, Coroutine, Optional, Callable
+from typing import Final
 from types import MappingProxyType
 
-from models.flags import CategoryFlag
-from models.response_models import ResponseHeader, ResponseBody
-from models.request_model import BaseHeaderComponent
+from models.flags import CategoryFlag, AuthFlags, PermissionFlags, InfoFlags, FileFlags
 
 from server.authz.auth_handler import top_auth_handler
 from server.info_ops.info_handler import top_info_handler
-from server.dependencies import ServerSingletonsRegistry
 from server.file_ops.file_handler import top_file_handler
 from server.permission_ops.permission_handler import top_permission_handler
+from server.info_ops import info_subhandlers
+from server.authz import auth_subhandlers
 
-__all__ = ('TOP_LEVEL_REQUEST_MAPPING',)
+from server.permission_ops import permission_subhandlers
+from server.file_ops import file_subhandlers
 
-TOP_LEVEL_REQUEST_MAPPING: MappingProxyType[int, Callable[[asyncio.StreamReader, BaseHeaderComponent, ServerSingletonsRegistry], Coroutine[Any, Any, tuple[ResponseHeader, Optional[ResponseBody]]]]] = (
-    MappingProxyType({CategoryFlag.AUTH         : top_auth_handler,
-                      CategoryFlag.INFO         : top_info_handler,
-                      CategoryFlag.FILE_OP      : top_file_handler,
-                      CategoryFlag.PERMISSION   : top_permission_handler}))
+from server.typing import AuthSubhandler, InfoSubhandler, FileSubhandler, PermissionSubhandler, TopRequestHandler
+
+__all__ = ('TOP_LEVEL_REQUEST_MAPPING',
+           'AUTH_SUBHABDLER_MAPPING',
+           'INFO_SUBHANDLER_MAPPING',
+           'PERMISSION_SUBHANDLER_MAPPING',
+           'FILE_SUBHANDLER_MAPPING')
+
+TOP_LEVEL_REQUEST_MAPPING: Final[MappingProxyType[CategoryFlag, TopRequestHandler]] = MappingProxyType(
+    {
+        CategoryFlag.AUTH : top_auth_handler,
+        CategoryFlag.INFO : top_info_handler,
+        CategoryFlag.FILE_OP : top_file_handler,
+        CategoryFlag.PERMISSION : top_permission_handler
+    }
+)
+
+AUTH_SUBHABDLER_MAPPING: Final[dict[AuthFlags, AuthSubhandler]] = {
+    AuthFlags.REGISTER : auth_subhandlers.handle_registration,
+    AuthFlags.LOGIN : auth_subhandlers.handle_login,
+    AuthFlags.REFRESH : auth_subhandlers.handle_session_refresh,
+    AuthFlags.LOGOUT : auth_subhandlers.handle_session_termination,
+    AuthFlags.CHANGE_PASSWORD : auth_subhandlers.handle_password_change,
+    AuthFlags.DELETE : auth_subhandlers.handle_deletion
+}
+
+INFO_SUBHANDLER_MAPPING: Final[dict[InfoFlags, InfoSubhandler]] = {
+    InfoFlags.HEARTBEAT : info_subhandlers.handle_heartbeat,
+    InfoFlags.PERMISSION_METADATA : info_subhandlers.handle_permission_query,
+    InfoFlags.FILE_METADATA : info_subhandlers.handle_filedata_query,
+    InfoFlags.USER_METADATA : info_subhandlers.handle_user_query,
+    InfoFlags.STORAGE_USAGE : info_subhandlers.handle_storage_query,
+    InfoFlags.SSL_CREDENTIALS : info_subhandlers.handle_ssl_query
+}
+
+PERMISSION_SUBHANDLER_MAPPING: Final[dict[PermissionFlags, PermissionSubhandler]] = {
+    PermissionFlags.GRANT : permission_subhandlers.grant_permission,
+    PermissionFlags.REVOKE : permission_subhandlers.revoke_permission,
+    PermissionFlags.HIDE : permission_subhandlers.hide_file,
+    PermissionFlags.PUBLICISE : permission_subhandlers.publicise_file,
+    PermissionFlags.TRANSFER : permission_subhandlers.publicise_file
+}
+
+# Write, append, and overwrite share the same handler
+FILE_SUBHANDLER_MAPPING: Final[dict[FileFlags, FileSubhandler]] = {
+    FileFlags.CREATE : file_subhandlers.handle_creation,
+    FileFlags.WRITE : file_subhandlers.handle_amendment,
+    FileFlags.APPEND : file_subhandlers.handle_amendment,
+    FileFlags.OVERWRITE : file_subhandlers.handle_amendment,
+    FileFlags.READ : file_subhandlers.handle_read,
+    FileFlags.DELETE : file_subhandlers.handle_deletion
+}
