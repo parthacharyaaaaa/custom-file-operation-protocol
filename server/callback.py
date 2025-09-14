4af6c95161ba07f3229aa496512c7700b7974084
Parth Acharya <1008parth@gmail.com>
2025-09-14 15:11:41 +0530

    Updates client connection callback task to comply with static type checking (Pyright)

--- server/callback.py
+++ server/callback.py
@@ -1,10 +1,12 @@
 import asyncio
 from traceback import format_exc, format_exception_only
+from typing import Optional
 
 from models.flags import CategoryFlag
 from models.request_model import BaseHeaderComponent
 from models.response_models import ResponseHeader
 from models.constants import REQUEST_CONSTANTS
+from models.typing import ProtocolComponent
 
 from server import errors
 from server import logging
@@ -21,22 +23,24 @@ async def callback(dependency_registry: ServerSingletonsRegistry,
                    reader: asyncio.StreamReader,
                    writer: asyncio.StreamWriter) -> None:
     while not reader.at_eof():
-        header_component: BaseHeaderComponent = None
+        header_component: Optional[ProtocolComponent] = None
         try:
-            header_component: BaseHeaderComponent = await process_component(n_bytes=REQUEST_CONSTANTS.header.max_bytesize,
-                                                                            reader=reader,
-                                                                            component_type='header',
-                                                                            timeout=dependency_registry.server_config.socket_connection_timeout)
+            header_component = await process_component(n_bytes=REQUEST_CONSTANTS.header.max_bytesize,
+                                                       reader=reader,
+                                                       component_type='header',
+                                                       timeout=dependency_registry.server_config.socket_connection_timeout)
+            
+            assert isinstance(header_component, BaseHeaderComponent)
             if not header_component:
                 raise errors.SlowStreamRate('Unable to parse header')
-            handler: RequestHandler = TOP_LEVEL_REQUEST_MAPPING.get(header_component.category)
+            handler: Optional[RequestHandler] = TOP_LEVEL_REQUEST_MAPPING.get(header_component.category)
             if not handler:
                 raise errors.UnsupportedOperation(f'Operation category must be in: {", ".join(CategoryFlag._member_names_)}')
 
             response_header, response_body = await handler(reader, header_component, dependency_registry)
-            body_stream: bytes = None
+            body_stream: Optional[bytes] = None
             if response_body:
-                body_stream: bytes = response_body.model_dump_json().encode('utf-8')
+                body_stream = response_body.model_dump_json().encode('utf-8')
                 response_header.body_size = len(body_stream)
             
             await send_response(writer=writer, header=response_header, body=body_stream)
@@ -52,7 +56,8 @@ async def callback(dependency_registry: ServerSingletonsRegistry,
             is_caught: bool = isinstance(e, errors.ProtocolException)
             if not is_caught:
                 asyncio.create_task(
-                    logging.enqueue_log(waiting_period=dependency_registry.server_config.log_waiting_period, queue=dependency_registry.log_queue,
+                    logging.enqueue_log(waiting_period=dependency_registry.server_config.log_waiting_period,
+                                        queue=dependency_registry.log_queue,
                                         log=db_models.ActivityLog(reported_severity=db_models.Severity.CRITICAL_FAILURE,
                                                                   log_category=db_models.LogType.INTERNAL,
                                                                   logged_by=db_models.LogAuthor.EXCEPTION_FALLBACK,
@@ -63,8 +68,9 @@ async def callback(dependency_registry: ServerSingletonsRegistry,
                 await writer.wait_closed()
                 return
             
+            assert header_component is None or isinstance(header_component, BaseHeaderComponent)
             connection_end: bool = False if not header_component else header_component.finish   # Explicit request to end connection from the client
-            response: ResponseHeader = ResponseHeader.from_protocol_exception(exc=e if is_caught else errors.InternalServerError,
+            response: ResponseHeader = ResponseHeader.from_protocol_exception(exc=e.__class__ if is_caught else errors.InternalServerError,
                                                                               version=dependency_registry.server_config.version if not header_component else header_component.version,
                                                                               end_conn=connection_end,
                                                                               host=str(dependency_registry.server_config.host),
