09e851e9166e9550cc89ed23b70f44f3c8ae951e
Parth Acharya <1008parth@gmail.com>
2025-09-15 18:04:07 +0530

    Updates __init__ and __main__ to comply with type checking

--- server/__init__.py
+++ server/__init__.py
@@ -2,6 +2,6 @@
 import os
 from dotenv import load_dotenv
 
-_loaded: bool = load_dotenv(os.path.join(__package__, '.env'))
+_loaded: bool = load_dotenv(os.path.join(str(__package__), '.env'))
 if not _loaded:
     raise RuntimeError('Failed to load env vars')
