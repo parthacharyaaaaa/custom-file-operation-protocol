8d61351ea81d5e72a18e42c47bb0d44877131cb8
Parth Acharya <1008parth@gmail.com>
2025-09-15 18:57:28 +0530

    Partially updates info subhandlers to comply with static type checkers
    Missing fixes for dict row cursor methods

--- server/info_ops/info_subhandlers.py
+++ server/info_ops/info_subhandlers.py
@@ -37,7 +37,10 @@ async def handle_heartbeat(header_component: BaseHeaderComponent,
                            server_config: ServerConfig) -> tuple[ResponseHeader, None]:
     '''Send a heartbeat signal back to the client'''
     return (
-        ResponseHeader.from_server(config=server_config, code=SuccessFlags.HEARTBEAT.value, version=header_component.version, ended_connection=header_component.finish),
+        ResponseHeader.from_server(config=server_config,
+                                   code=SuccessFlags.HEARTBEAT,
+                                   version=header_component.version,
+                                   ended_connection=header_component.finish),
         None
     )
 
@@ -46,10 +49,13 @@ async def handle_permission_query(header_component: BaseHeaderComponent,
                                   info_component: BaseInfoComponent,
                                   connection_master: ConnectionPoolManager,
                                   server_config: ServerConfig) -> tuple[ResponseHeader, ResponseBody]:
+    if not info_component.subject_resource:
+        raise errors.InvalidBodyValues(f'Missing resource name')
+    
     owner, filename = derive_file_identity(info_component.subject_resource)
     async with await connection_master.request_connection(1) as proxy:
         if not await db_utils.check_file_permission(filename=filename, owner=owner, grantee=auth_component.identity,
-                                                    check_for=db_models.FilePermissions.MANAGE_RW.value,
+                                                    check_for=db_models.FilePermissions.MANAGE_RW,
                                                     connection_master=connection_master, proxy=proxy):
             raise errors.InsufficientPermissions
         
@@ -58,7 +64,7 @@ async def handle_permission_query(header_component: BaseHeaderComponent,
                                  (owner, filename))
             result_set: list[dict[str, Any]] = await cursor.fetchall()
 
-            return (ResponseHeader.from_server(server_config, SuccessFlags.SUCCESSFUL_QUERY_ANSWER.value, ended_connection=header_component.finish),
+            return (ResponseHeader.from_server(server_config, SuccessFlags.SUCCESSFUL_QUERY_ANSWER, ended_connection=header_component.finish),
                     ResponseBody(contents={result.pop('grantee') : result for result in result_set}))
 
 async def handle_filedata_query(header_component: BaseHeaderComponent,
@@ -66,10 +72,13 @@ async def handle_filedata_query(header_component: BaseHeaderComponent,
                                 info_component: BaseInfoComponent,
                                 connection_master: ConnectionPoolManager,
                                 server_config: ServerConfig) -> tuple[ResponseHeader, ResponseBody]:
+    if not info_component.subject_resource:
+        raise errors.InvalidBodyValues(f'Missing resource name')
+    
     owner, filename = derive_file_identity(info_component.subject_resource)
     async with await connection_master.request_connection(1) as proxy:
         if not await db_utils.check_file_permission(filename=filename, owner=owner, grantee=auth_component.identity,
-                                                     check_for=db_models.FilePermissions.MANAGE_RW.value,
+                                                     check_for=db_models.FilePermissions.MANAGE_RW,
                                                      connection_master=connection_master, proxy=proxy):
             raise errors.InsufficientPermissions
         
@@ -81,7 +90,7 @@ async def handle_filedata_query(header_component: BaseHeaderComponent,
             if header_component.subcategory & InfoFlags.VERBOSE:
                 file_data |= get_local_filedata(server_config.files_directory.joinpath(owner, filename))
 
-            return (ResponseHeader.from_server(server_config, SuccessFlags.SUCCESSFUL_QUERY_ANSWER.value, ended_connection=header_component.finish),
+            return (ResponseHeader.from_server(server_config, SuccessFlags.SUCCESSFUL_QUERY_ANSWER, ended_connection=header_component.finish),
                     ResponseBody(contents=file_data))
 
 async def handle_user_query(header_component: BaseHeaderComponent,
@@ -105,7 +114,7 @@ async def handle_user_query(header_component: BaseHeaderComponent,
                                                     await cursor.fetchall()}}
                 
 
-            return (ResponseHeader.from_server(server_config, SuccessFlags.SUCCESSFUL_QUERY_ANSWER.value, ended_connection=header_component.finish),
+            return (ResponseHeader.from_server(server_config, SuccessFlags.SUCCESSFUL_QUERY_ANSWER, ended_connection=header_component.finish),
                     ResponseBody(contents=user_data))
 
 async def handle_storage_query(header_component: BaseHeaderComponent,
@@ -115,7 +124,7 @@ async def handle_storage_query(header_component: BaseHeaderComponent,
     storage_data: dict[str, Any] = await asyncio.wait_for(scan_task, 10)
     storage_data.update({'storage_left' : server_config.user_max_storage - storage_data['storage_used'],
                          'files_left' : server_config.user_max_files - storage_data['files_made']})
-    return (ResponseHeader.from_server(server_config, SuccessFlags.SUCCESSFUL_QUERY_ANSWER.value, ended_connection=header_component.finish),
+    return (ResponseHeader.from_server(server_config, SuccessFlags.SUCCESSFUL_QUERY_ANSWER, ended_connection=header_component.finish),
             ResponseBody(contents=storage_data))
 
 async def handle_ssl_query(server_config: ServerConfig) -> tuple[ResponseHeader, ResponseBody]:
@@ -123,5 +132,5 @@ async def handle_ssl_query(server_config: ServerConfig) -> tuple[ResponseHeader,
     if server_config.rollover_data_filepath.exists() and (data:=server_config.rollover_data_filepath.read_bytes()):
         rollover_data = orjson.loads(data)
     
-    return (ResponseHeader.from_server(server_config, SuccessFlags.SUCCESSFUL_QUERY_ANSWER.value),
+    return (ResponseHeader.from_server(server_config, SuccessFlags.SUCCESSFUL_QUERY_ANSWER),
             ResponseBody(contents={'rollover_data' : rollover_data}))
