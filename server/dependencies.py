6b003a3f11ed131def27728147be7ae6993155b5
Parth Acharya <1008parth@gmail.com>
2025-09-15 18:03:40 +0530

    Updates signature and return type of inject_global_singletons

--- server/dependencies.py
+++ server/dependencies.py
@@ -2,7 +2,7 @@ import asyncio
 import inspect
 from functools import partial
 from types import MappingProxyType
-from typing import Any, NewType, Callable, Optional, Annotated, TypeAlias, Union
+from typing import Any, Callable, Coroutine, NewType, Optional, TypeAlias, Union
 
 from aiofiles.threadpool.binary import AsyncBufferedReader, AsyncBufferedIOBase
 
@@ -16,6 +16,7 @@ from server.config.server_config import ServerConfig
 from server.database import models as db_models
 from server.database.connections import ConnectionPoolManager
 from server.file_ops.storage import StorageCache
+from server.typing import SubhandlerResponse
 
 import pydantic
 
@@ -36,7 +37,7 @@ _singleton_registry_config_dict: pydantic.ConfigDict = pydantic.ConfigDict(
 
 
 # NewType annotations for global singletons with data types that can be repeated in a function signature
-GlobalLogQueueType = NewType('GlobalLogQueueType', asyncio.queues.Queue[db_models.ActivityLog])
+GlobalLogQueueType = NewType('GlobalLogQueueType', asyncio.Queue[db_models.ActivityLog])
 GlobalReadCacheType = NewType('GlobalReadCacheType', TTLCache[str, dict[str, AsyncBufferedReader]])
 GlobalAmendCacheType = NewType('GlobalAmendCacheType', TTLCache[str, dict[str, AsyncBufferedIOBase]])
 GlobalDeleteCacheType = NewType('GlobalDeleteCacheType', TTLCache[str, str])
@@ -58,16 +59,16 @@ SingletonTypes: TypeAlias = Union[
 class ServerSingletonsRegistry(metaclass=SingletonMetaclass):
     # Custom singleton classes are not assigned an explicit NewType 
     # since it is impossible to have a function signature having duplicate type annotations for them
-    server_config: Annotated[ServerConfig, pydantic.Field(frozen=True)]
-    user_manager: Annotated[UserManager, pydantic.Field(frozen=True)]
-    connection_pool_manager: Annotated[ConnectionPoolManager, pydantic.Field(frozen=True)]
-    storage_cache: Annotated[StorageCache, pydantic.Field(frozen=True)]
+    server_config: ServerConfig = pydantic.Field(frozen=True)
+    user_manager: UserManager = pydantic.Field(frozen=True)
+    connection_pool_manager: ConnectionPoolManager = pydantic.Field(frozen=True)
+    storage_cache: StorageCache = pydantic.Field(frozen=True)
 
-    log_queue: Annotated[GlobalLogQueueType, pydantic.Field(frozen=True)]
-    reader_cache: Annotated[GlobalReadCacheType, pydantic.Field(frozen=True)]
-    amendment_cache: Annotated[GlobalAmendCacheType, pydantic.Field(frozen=True)]
-    deletion_cache: GlobalDeleteCacheType
-    file_locks: GlobalFileLockType
+    log_queue: asyncio.Queue[db_models.ActivityLog] = pydantic.Field(frozen=True)
+    reader_cache: TTLCache[str, dict[str, AsyncBufferedReader]] = pydantic.Field(frozen=True)
+    amendment_cache: TTLCache[str, dict[str, AsyncBufferedIOBase]] = pydantic.Field(frozen=True)
+    deletion_cache: TTLCache[str, str]
+    file_locks: TTLCache[str, bytes]
 
     _registry_reverse_mapping: MappingProxyType[SingletonTypes, Any] = pydantic.PrivateAttr(default=MappingProxyType({}))
 
@@ -89,9 +90,9 @@ class ServerSingletonsRegistry(metaclass=SingletonMetaclass):
         return self._registry_reverse_mapping
 
     def inject_global_singletons(self,
-                                 func: Callable[..., tuple[ResponseHeader, Optional[ResponseBody]]],
+                                 func: Callable[..., SubhandlerResponse],
                                  strict: bool = False,
-                                 **overrides_kwargs) -> Callable[..., tuple[ResponseHeader, Optional[ResponseBody]]]:
+                                 **overrides_kwargs) -> Callable[..., SubhandlerResponse]:
         bound_args: dict[str, Any] = {}
         for paramname, paramtype in inspect.signature(func).parameters.items():
             if paramname in overrides_kwargs:
