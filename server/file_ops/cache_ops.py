a0112a6d773389d69f9327cebecf3a42cde791b2
Parth Acharya <1008parth@gmail.com>
2025-09-15 19:06:30 +0530

    Updates file cache methods to comply with standard type checking

--- server/file_ops/cache_ops.py
+++ server/file_ops/cache_ops.py
@@ -3,30 +3,35 @@
 from cachetools import TTLCache
 from aiofiles.threadpool.binary import AsyncBufferedReader
 from aiofiles.threadpool.binary import AsyncBufferedIOBase, AsyncBufferedReader
-from typing import Literal, Union
+from typing import Literal, Optional, Union
 
 __all__ = ('remove_reader', 'get_reader', 'purge_file_entries', 'rename_file_entries')
 
-def remove_reader(read_cache: TTLCache[str, dict[str, AsyncBufferedReader]], fpath: str, identifier: str) -> None:
+def remove_reader(read_cache: TTLCache[str, dict[str, Union[AsyncBufferedReader, AsyncBufferedIOBase]]],
+                  fpath: str, identifier: str) -> None:
     try:
         read_cache[fpath].pop(identifier, None)
     except KeyError:
         return None
     
-def get_reader(read_cache: TTLCache[str, dict[str, AsyncBufferedReader]], fpath: str, identifier: str) -> AsyncBufferedReader:
+def get_reader(read_cache: TTLCache[str, dict[str, AsyncBufferedReader]],
+               fpath: str, identifier: str) -> Optional[AsyncBufferedReader]:
     try:
         return read_cache[fpath][identifier]
     except KeyError:
         return None
     
-async def purge_file_entries(fpath: str, deleted_cache: TTLCache[str, Literal[True]], *cache_list: TTLCache[str, dict[str, Union[AsyncBufferedIOBase, AsyncBufferedReader]]]) -> None:
+async def purge_file_entries(fpath: str,
+                             deleted_cache: TTLCache[str, Literal[True]],
+                             *cache_list: TTLCache[str, dict[str, Union[AsyncBufferedIOBase, AsyncBufferedReader]]]) -> None:
     deleted_cache.update({fpath:True})
     for cache in cache_list:
         buffered_mapping = cache.pop(fpath, {})
         for _, buffered_obj in buffered_mapping.items():
             await buffered_obj.close()
 
-def rename_file_entries(old_fpath: str, new_fpath: str, *cache_list: TTLCache[str, dict[str, Union[AsyncBufferedIOBase, AsyncBufferedReader]]]) -> None:
+def rename_file_entries(old_fpath: str, new_fpath: str,
+                        *cache_list: TTLCache[str, dict[str, Union[AsyncBufferedIOBase, AsyncBufferedReader]]]) -> None:
     for cache in cache_list:
         buffered_mapping = cache.pop(old_fpath, {})
         cache[new_fpath] = buffered_mapping
