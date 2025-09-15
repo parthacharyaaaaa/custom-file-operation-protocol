65244643127d457b24fa10803c54bedf7ea96152
Parth Acharya <1008parth@gmail.com>
2025-09-15 19:43:55 +0530

    Adds BaseInfoComponent to ProtocolComponent alias

--- models/typing.py
+++ models/typing.py
@@ -2,9 +2,9 @@
 from typing import TypeAlias, Union
 
 from models.flags import AuthFlags, InfoFlags, FileFlags, PermissionFlags
-from models.request_model import BaseFileComponent, BaseAuthComponent, BaseHeaderComponent, BasePermissionComponent
+from models.request_model import BaseFileComponent, BaseAuthComponent, BaseHeaderComponent, BasePermissionComponent, BaseInfoComponent
 from models.response_codes import SuccessFlags, ClientErrorFlags, ServerErrorFlags
 
 SubcategoryFlag:        TypeAlias = Union[AuthFlags, InfoFlags, FileFlags, PermissionFlags]
-ProtocolComponent:      TypeAlias = Union[BaseHeaderComponent, BaseAuthComponent, BaseFileComponent, BasePermissionComponent]
+ProtocolComponent:      TypeAlias = Union[BaseHeaderComponent, BaseAuthComponent, BaseFileComponent, BasePermissionComponent, BaseInfoComponent]
 ResponseCode:           TypeAlias = Union[SuccessFlags, ClientErrorFlags, ServerErrorFlags]
