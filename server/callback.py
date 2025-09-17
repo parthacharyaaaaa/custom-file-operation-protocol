f0ce4922238fd9d5ec4c54c6274092f558703322
Parth Acharya <1008parth@gmail.com>
2025-09-17 21:43:01 +0530

    Updates client connection callback task to accept request_handler_mapping as arg

--- server/callback.py
+++ server/callback.py
@@ -1,5 +1,6 @@
 import asyncio
 from traceback import format_exc, format_exception_only
+from types import MappingProxyType
 from typing import Optional, TYPE_CHECKING
 
 from models.flags import CategoryFlag
@@ -14,14 +15,14 @@ from server.comms_utils.incoming import process_component
 from server.comms_utils.outgoing import send_response
 from server.database import models as db_models
 from server.dependencies import ServerSingletonsRegistry
-from server.dispatch import TOP_LEVEL_REQUEST_MAPPING
-from server.typing import PartialRequestHandler
+from server.typing import PartialisedRequestHandler
 
 __all__ = ('callback',)
 
 if TYPE_CHECKING: assert REQUEST_CONSTANTS
 
 async def callback(dependency_registry: ServerSingletonsRegistry,
+                   request_handler_mapping: MappingProxyType[CategoryFlag, PartialisedRequestHandler],
                    reader: asyncio.StreamReader,
                    writer: asyncio.StreamWriter) -> None:
     while not reader.at_eof():
@@ -32,14 +33,17 @@ async def callback(dependency_registry: ServerSingletonsRegistry,
                                                        component_type='header',
                                                        timeout=dependency_registry.server_config.socket_connection_timeout)
             
-            assert isinstance(header_component, BaseHeaderComponent)
             if not header_component:
                 raise errors.SlowStreamRate('Unable to parse header')
-            handler: Optional[PartialRequestHandler] = TOP_LEVEL_REQUEST_MAPPING.get(header_component.category)
+            assert isinstance(header_component, BaseHeaderComponent)
+
+            handler: Optional[PartialisedRequestHandler] = request_handler_mapping.get(header_component.category)
             if not handler:
                 raise errors.UnsupportedOperation(f'Operation category must be in: {", ".join(CategoryFlag._member_names_)}')
 
-            response_header, response_body = await handler(reader, header_component, dependency_registry)
+            response_header, response_body = await handler(stream_reader=reader,
+                                                           header_component=header_component,
+                                                           server_singleton_registry=dependency_registry)
             body_stream: Optional[bytes] = None
             if response_body:
                 body_stream = response_body.model_dump_json().encode('utf-8')
