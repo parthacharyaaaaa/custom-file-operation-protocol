e7e0615590bbdd2c9dde6a4dc3f977a1ccaac10e
Parth Acharya <1008parth@gmail.com>
2025-09-17 21:40:52 +0530

    Fixes bug in model __init__ where REQUEST_CONSTANTS & RESPONSE_CONSTANTS were being imported before initialization

--- models/__init__.py
+++ models/__init__.py
@@ -1,5 +1,6 @@
 '''Models and objects shared between server and client'''
 
-from models.constants import load_constants, REQUEST_CONSTANTS, RESPONSE_CONSTANTS
+from models.constants import load_constants
 load_constants()
+from models.constants import REQUEST_CONSTANTS, RESPONSE_CONSTANTS
 assert REQUEST_CONSTANTS and RESPONSE_CONSTANTS
