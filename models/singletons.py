84725117ce2c3d5c0ee05bdf01f11e811c3f1228
Parth Acharya <1008parth@gmail.com>
2025-09-18 18:12:56 +0530

    Fixes memory leak in SingletonMetaclass metaclass
    references to the instantiated object are now weak

--- models/singletons.py
+++ models/singletons.py
@@ -1,12 +1,20 @@
-from typing import Any
+'''Singleton metaclass support with runtime enforcement'''
+from typing import Any, Optional
 import warnings
+import weakref
+
+__all__ = 'SingletonMetaclass',
 
 class SingletonMetaclass(type):
-    _instance: Any = None
+    _instance_reference: Optional[weakref.ReferenceType[Any]] = None
 
     def __call__(cls, *args, **kwargs):
-        if not cls._instance:
-            cls._instance = super().__call__(*args, **kwargs)
+        if not cls._instance_reference:
+            # A temporary strong reference is needed for this method's lifetime, 
+            # otherwise the object gets garbage collected before being returned to the constructor's assignee
+            instance = super().__call__(*args, **kwargs)
+            cls._instance_reference = weakref.ref(instance, lambda _ : setattr(cls, "_instance_reference", None))
         else:
             warnings.warn('Attempt to instantiate a singleton class more than once, rejecting...', category=RuntimeWarning)
-        return cls._instance
+        
+        return cls._instance_reference()
