512695be234dd2de2789de98758cdf9db0d34012
Parth Acharya <1008parth@gmail.com>
2025-09-19 19:50:02 +0530

    Updates client side auth operations to comply with standard Pyright type checking

--- client/operations/auth_operations.py
+++ client/operations/auth_operations.py
@@ -1,7 +1,7 @@
 '''Methods corresponding to client auth'''
 
 import asyncio
-from typing import Any
+from typing import Any, Optional
 
 from client import session_manager
 from client.auxillary import operational_utils
@@ -29,19 +29,22 @@ __all__ = ('create_remote_user',
 async def create_remote_user(reader: asyncio.StreamReader, writer: asyncio.StreamWriter,
                              auth_component: BaseAuthComponent,
                              client_config: client_constants.ClientConfig, session_manager: session_manager.SessionManager,
-                             display_credentials: bool = False, end_connection: bool = False) -> None:
+                             end_connection: bool = False) -> None:
     header_component: BaseHeaderComponent = operational_utils.make_header_component(client_config, session_manager, CategoryFlag.AUTH, AuthFlags.REGISTER, finish=end_connection)
     await send_request(writer=writer,
                        header_component=header_component,
                        auth_component=auth_component)
     
     response_header, response_body = await process_response(reader, writer, client_config.read_timeout)
-    if response_header.code != SuccessFlags.SUCCESSFUL_USER_CREATION.value:
+    if response_header.code != SuccessFlags.SUCCESSFUL_USER_CREATION:
         await display(auth_messages.failed_auth_operation(operation=AuthFlags.REGISTER, code=response_header.code))
         return
-    
-    epoch, username = await operational_utils.filter_claims(response_body.contents, "epoch", "username")
-    await display(auth_messages.successful_user_creation(username or auth_component.identity, epoch))
+    if not (response_body and response_body.contents):
+        await display(general_messages.malformed_response_body('Missing response body'))
+        await display(auth_messages.successful_user_creation(auth_component.identity))
+    else:
+        epoch, username = await operational_utils.filter_claims(response_body.contents, "epoch", "username")
+        await display(auth_messages.successful_user_creation(username or auth_component.identity, epoch))
 
 async def delete_remote_user(reader: asyncio.StreamReader, writer: asyncio.StreamWriter,
                              auth_component: BaseAuthComponent,
@@ -53,13 +56,16 @@ async def delete_remote_user(reader: asyncio.StreamReader, writer: asyncio.Strea
                        auth_component=auth_component)
     
     response_header, response_body = await process_response(reader, writer, client_config.read_timeout)
-    if response_header.code != SuccessFlags.SUCCESSFUL_USER_DELETION.value:
+    if response_header.code != SuccessFlags.SUCCESSFUL_USER_DELETION:
         await display(auth_messages.failed_auth_operation(AuthFlags.DELETE, response_header.code))
+        return
+    if not (response_body and response_body.contents):
+        await display(general_messages.malformed_response_body('Missing response body and contents'))
         return
 
     deleted_count, deleted_files = await operational_utils.filter_claims(response_body.contents, "deleted_count", "deleted_files")
-    if actual_fcount:=len(deleted_files) != deleted_count:
-        await display(general_messages.malformed_response_body(message=auth_messages.filecount_mismatch(deleted_count, actual_fcount)))
+    if len(deleted_files) != deleted_count:
+        await display(general_messages.malformed_response_body(message=auth_messages.filecount_mismatch(deleted_count, len(deleted_files))))
 
     await display(auth_messages.successful_user_deletion(auth_component.identity, deleted_count, deleted_files))
 
@@ -73,11 +79,14 @@ async def authorize(reader: asyncio.StreamReader, writer: asyncio.StreamWriter,
                        auth_component=auth_component)
     
     response_header, response_body = await process_response(reader, writer, client_config.read_timeout)
-    if response_header.code != SuccessFlags.SUCCESSFUL_AUTHENTICATION.value:
+    if response_header.code != SuccessFlags.SUCCESSFUL_AUTHENTICATION:
         await display(auth_messages.failed_auth_operation(AuthFlags.LOGIN, response_header.code))
         return
+    if not (response_body and response_body.contents):
+        await display(general_messages.malformed_response_body("Missing response body"))
+        return
     
-    session_dict: dict[str, Any] = response_body.contents.get('session')
+    session_dict: Optional[dict[str, Any]] = response_body.contents.get('session')
     if not session_dict:
         await display(auth_messages.failed_auth_operation(AuthFlags.LOGIN, response_header.code), general_messages.missing_response_claim('session'), sep=b'\n')
         return
@@ -87,23 +96,33 @@ async def authorize(reader: asyncio.StreamReader, writer: asyncio.StreamWriter,
         return
 
     session_manager.local_authenticate(identity=auth_component.identity, **session_dict)
+    assert session_manager.session_metadata
     await display(auth_messages.successful_authorization(remote_user=auth_component.identity))
     if display_credentials:
         await display(format_dict(session_manager.session_metadata.json_repr))
     
 async def reauthorize(reader: asyncio.StreamReader, writer: asyncio.StreamWriter,
-                      client_config: client_constants.ClientConfig, session_manager: session_manager.SessionManager,
+                      client_config: client_constants.ClientConfig,
+                      session_manager: session_manager.SessionManager,
                       display_credentials: bool = False) -> None:
-    await send_request(writer, BaseHeaderComponent(version=client_config.version, category=CategoryFlag.AUTH, subcategory=AuthFlags.REFRESH))
+    if not session_manager.check_authentication_integrity():
+        raise ValueError(f'Cannot reauthorize when session manager holds no credentials')
+    assert session_manager.session_metadata and session_manager.identity
+    
+    await send_request(writer=writer,
+                       header_component=operational_utils.make_header_component(client_config, session_manager, CategoryFlag.AUTH, AuthFlags.REFRESH))
     response_header, response_body = await process_response(reader, writer, client_config.read_timeout)
 
     if response_header.code != SuccessFlags.SUCCESSFUL_SESSION_REFRESH:
         await display(auth_messages.failed_auth_operation(AuthFlags.REFRESH, response_header.code))
         return
+    if not (response_body and response_body.contents):
+        await display(general_messages.malformed_response_body('Missing response body'))
+        return
     
     new_digest, iteration = await operational_utils.filter_claims(response_body.contents, "digest", "iteration")
     if not new_digest:
-        await display(auth_messages.failed_auth_operation(AuthFlags.REFRESH, ServerErrorFlags.INTERNAL_SERVER_ERROR.value))
+        await display(auth_messages.failed_auth_operation(AuthFlags.REFRESH, ServerErrorFlags.INTERNAL_SERVER_ERROR))
         return
     
     session_manager.session_metadata.update_digest(new_digest=new_digest)
@@ -119,24 +138,34 @@ async def reauthorize(reader: asyncio.StreamReader, writer: asyncio.StreamWriter
 async def end_remote_session(reader: asyncio.StreamReader, writer: asyncio.StreamWriter,
                              client_config: client_constants.ClientConfig, session_manager: session_manager.SessionManager,
                              display_credentials: bool = False, end_connection: bool = False) -> None:
+    if not session_manager.check_authentication_integrity():
+        raise ValueError(f'Cannot reauthorize when session manager holds no credentials')
+    assert session_manager.session_metadata and session_manager.identity
+
     header_component: BaseHeaderComponent = operational_utils.make_header_component(client_config, session_manager, CategoryFlag.AUTH, AuthFlags.LOGOUT, finish=end_connection)
     await send_request(writer=writer,
                        header_component=header_component,
                        auth_component=session_manager.auth_component)
     response_header, response_body = await process_response(reader, writer, client_config.read_timeout)
     
-    if response_header.code != SuccessFlags.SUCCESSFUL_SESSION_TERMINATION.value:
+    if response_header.code != SuccessFlags.SUCCESSFUL_SESSION_TERMINATION:
         await display(auth_messages.failed_auth_operation(AuthFlags.LOGOUT, response_header.code))
         return
     
     remote_user: str = session_manager.identity
     session_manager.clear_auth_data()
-
+    if not (response_body and response_body.contents):
+        await display(auth_messages.successful_logout(remote_user=remote_user))
+        return
     await display(auth_messages.successful_logout(remote_user=remote_user, **(response_body.contents if display_credentials else {})))
 
 async def change_password(reader: asyncio.StreamReader, writer: asyncio.StreamWriter,
                           new_password: str,
                           client_config: client_constants.ClientConfig, session_manager: session_manager.SessionManager) -> None:
+    if not session_manager.check_authentication_integrity():
+        raise ValueError(f'Cannot reauthorize when session manager holds no credentials')
+    assert session_manager.session_metadata and session_manager.identity and session_manager.auth_component
+
     if not (session_manager.auth_component.token and session_manager.auth_component.refresh_digest):
         await display(auth_messages.invalid_user_data(ValidationError("Token and refresh digest required")))
         return
@@ -147,16 +176,21 @@ async def change_password(reader: asyncio.StreamReader, writer: asyncio.StreamWr
         await display(auth_messages.invalid_user_data(v))
         return
 
-    await send_request(writer,
-                       header_component=BaseHeaderComponent(version=client_config.version, category=CategoryFlag.AUTH, subcategory=AuthFlags.CHANGE_PASSWORD),
+    await send_request(writer=writer,
+                       header_component=operational_utils.make_header_component(client_config, session_manager, CategoryFlag.AUTH, AuthFlags.CHANGE_PASSWORD),
                        auth_component=auth_component)
     
     response_header, response_body = await process_response(reader, writer, client_config.read_timeout)
 
-    if response_header.code != SuccessFlags.SUCCESSFUL_PASSWORD_CHANGE.value:
+    if response_header.code != SuccessFlags.SUCCESSFUL_PASSWORD_CHANGE:
         await display(auth_messages.failed_auth_operation(AuthFlags.CHANGE_PASSWORD, response_header.code))
         return
 
     # Successful password changes require reauthorization
     session_manager.clear_auth_data()
-    await display(response_body.contents.get('message', 'Remote session terminated, please reauthorize...'))
+    output_str: str = 'Remote session terminated, please reauthorize...'
+    if not (response_body and response_body.contents):
+        await display(output_str)
+        return
+    
+    await display(response_body.contents.get('message', output_str))
